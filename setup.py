fd513f7364c027c0238d215d3f0ce499e8709720
rlratzel <rlratzel@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-05-14 22:31:50 +0000

    * Copying new setuptools-based setup.py from enthought_split branch.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'enthought.traits.ui.wx',
+    version = '1.1.0',
+    description  = 'WxPython backend for enthought.traits',
+    author       = 'David C. Morrill',
+    author_email = 'dmorrill@enthought.com',
+    url          = 'http://code.enthought.com/traits',
+    license      = 'BSD',
+    zip_safe     = False,
+    packages = find_packages(),
+    include_package_data = True,
+    install_requires = [
+        #'wx',  # fixme: not available as an egg on all platforms.
+        "enthought.model",
+        "enthought.traits",
+    ],
+    namespace_packages = [
+        "enthought",
+        "enthought.traits",
+        "enthought.traits.ui",
+    ],
+)
