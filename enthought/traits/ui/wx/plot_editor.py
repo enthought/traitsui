2d9220feb1972d8fd9d39fab4163bfbe5f22d7c9
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-07-04 21:09:40 +0000

    * Refactored to reduce Traits dependencies on Chaco, Enable, and Kiva. * Removed the PlotEditor from Traits -- Chaco2 already has its own version. * Moved the KivaFontTrait and KivaFontEditor out of Traits and into Kiva. * Moved the EnableRGBAColorEditor out of Traits and into Enable.
    As these changes were done over multiple sessions, I may need to review the diffs and do another checkin to get everything straight.

--- enthought/traits/ui/wx/plot_editor.py
+++ enthought/traits/ui/wx/plot_editor.py
@@ -2,19 +2,19 @@
 #
 #  Copyright (c) 2005, Enthought, Inc.
 #  All rights reserved.
-#  
+#
 #  This software is provided without warranty under the terms of the BSD
 #  license included in enthought/LICENSE.txt and may be redistributed only
 #  under the conditions described in the aforementioned license.  The license
 #  is also available online at http://www.enthought.com/licenses/BSD.txt
 #  Thanks for using Enthought open source!
-#  
+#
 #  Author: David C. Morrill
 #  Date: 01/14/2005
 #
 #------------------------------------------------------------------------------
 
-""" Defines the plot editor and the plot editor factory, for the wxPython user 
+""" Defines the plot editor and the plot editor factory, for the wxPython user
 interface toolkit.
 """
 
@@ -24,39 +24,42 @@ interface toolkit.
 
 import wx
 
+from enthought.enable2.traits.ui.wx.enable_rgba_color_editor import \
+    EnableRGBAColorEditor
+
 from enthought.traits.api \
     import List, Str, Bool, Range, Tuple, RGBAColor
-    
+
 from enthought.traits.ui.api \
-    import View, Item, EnableRGBAColorEditor
-    
+    import View, Item
+
 from editor_factory \
     import EditorFactory
-    
+
 from editor \
     import Editor
 
 #-------------------------------------------------------------------------------
-#  Constants:  
+#  Constants:
 #-------------------------------------------------------------------------------
 
 WindowColor = ( 236.0 / 255.0, 233.0 / 255.0, 216.0 / 255.0, 1.0 )
 
 #-------------------------------------------------------------------------------
-#  Trait definitions:  
+#  Trait definitions:
 #-------------------------------------------------------------------------------
 
 # Range of values for an axis
-AxisRange =  Tuple( ( 0.0, 1.0, 0.01 ), 
+AxisRange =  Tuple( ( 0.0, 1.0, 0.01 ),
                     labels = [ 'Low', 'High', 'Step' ],
                     cols   = 3 )
-                    
-# Minimum and maximum axis bounds                    
+
+# Minimum and maximum axis bounds
 AxisBounds = Tuple( ( 0.0, 1.0 ),
                     labels = [ 'Min', 'Max' ],
                     cols   = 2 )
 
-# Height and width range for the plot widget                   
+# Height and width range for the plot widget
 PlotSize = Range( 50, 1000, 180 )
 
 # Range of plot line weights
@@ -72,69 +75,69 @@ color_editor = EnableRGBAColorEditor( auto_set = False )
 class ToolkitEditorFactory ( EditorFactory ):
     """ wxPython editor factory for plot editors.
     """
-    
+
     #---------------------------------------------------------------------------
     #  Trait definitions:
     #---------------------------------------------------------------------------
-    
+
     # List of traits the plot is associated with:
-    traits = List( Str )  
-    
+    traits = List( Str )
+
     # Plot title:
     title = Str
-    
+
     # X-axis label:
-    x_label = Str 
-    
+    x_label = Str
+
     # Y-axis label:
-    y_label = Str 
-    
+    y_label = Str
+
     # X-axis range:
     x_range = AxisRange
-    
+
     # Name of handler method to compute x values:
-    x_values = Str          
-    
+    x_values = Str
+
     # Name of handler method to compute y values:
-    y_values = Str          
-    
+    y_values = Str
+
     # Should the X-axis bounds be automatically computed?
     x_auto = Bool( True )
-    
+
     # X-axis bounds (i.e., minimum and maximum values):
     x_bounds = AxisBounds
-    
+
     # Should the Y-axis bounds be automatically computed?
     y_auto = Bool( True )
-    
+
     # Y-axis bounds (i.e., minimum and maximum values):
-    y_bounds = AxisBounds   
-    
+    y_bounds = AxisBounds
+
     # Width of the plot editor:
-    width = PlotSize     
-    
+    width = PlotSize
+
     # Height of the plot editor:
-    height = PlotSize     
-    
+    height = PlotSize
+
     # Weight of plot line (i.e. line thickness):
-    weight = LineWeight   
-    
-    # Line color: 
+    weight = LineWeight
+
+    # Line color:
     color = RGBAColor( 'blue',  editor = color_editor )
-    
+
     # Background color:
     bg_color = RGBAColor( 'white', editor = color_editor )
-    
+
     #---------------------------------------------------------------------------
-    #  Traits view definition:    
+    #  Traits view definition:
     #---------------------------------------------------------------------------
-        
+
     traits_view = View( [ [ 'traits',
                             '|[Trait Names]<>' ],
-                          [ 'title', 'x_label', 'y_label', 
+                          [ 'title', 'x_label', 'y_label',
                             '|[Labels]' ],
                           [ 'y_values', '_', 'x_values',
-                            Item( 'x_range', 
+                            Item( 'x_range',
                                   enabled_when = "object.x_values == ''" ),
                             '|[Plot Values]' ],
                           [ [ 'x_auto',
@@ -148,56 +151,56 @@ class ToolkitEditorFactory ( EditorFactory ):
                             '|[Plot bounds]' ],
                           [ 'width', 'height',
                             '-[Plot Size]' ],
-                          [ 'weight{Line Weight}', '_', 
-                            'color{Line Color}', 
+                          [ 'weight{Line Weight}', '_',
+                            'color{Line Color}',
                             'bg_color{Background color}',
                             '|[Plot Attributes]' ]
                         ] )
-    
+
     #---------------------------------------------------------------------------
     #  'Editor' factory methods:
     #---------------------------------------------------------------------------
-    
+
     def simple_editor ( self, ui, object, name, description, parent ):
         return PlotEditor( parent,
-                           factory     = self, 
-                           ui          = ui, 
-                           object      = object, 
-                           name        = name, 
+                           factory     = self,
+                           ui          = ui,
+                           object      = object,
+                           name        = name,
                            description = description )
-    
+
     def text_editor ( self, ui, object, name, description, parent ):
         return PlotEditor( parent,
-                           factory     = self, 
-                           ui          = ui, 
-                           object      = object, 
-                           name        = name, 
+                           factory     = self,
+                           ui          = ui,
+                           object      = object,
+                           name        = name,
                            description = description )
-    
+
     def readonly_editor ( self, ui, object, name, description, parent ):
         return PlotEditor( parent,
-                           factory     = self, 
-                           ui          = ui, 
-                           object      = object, 
-                           name        = name, 
+                           factory     = self,
+                           ui          = ui,
+                           object      = object,
+                           name        = name,
                            description = description )
 
 #-------------------------------------------------------------------------------
-#  'PlotEditor' class  
+#  'PlotEditor' class
 #-------------------------------------------------------------------------------
-                     
+
 class PlotEditor ( Editor ):
     """ Editor for displaying trait values in a Chaco plot.
     """
-    
+
     # Indicate that the plot can use extra space in the view:
     scrollable = True
-    
+
     #---------------------------------------------------------------------------
     #  Finishes initializing the editor by creating the underlying toolkit
     #  widget:
     #---------------------------------------------------------------------------
-        
+
     def init ( self, parent ):
         """ Finishes initializing the editor by creating the underlying toolkit
             widget.
@@ -207,7 +210,7 @@ class PlotEditor ( Editor ):
 
         from enthought.enable.wx \
             import Window
-            
+
         factory = self.factory
         x_axis  = PlotAxis()
         if not factory.x_auto:
@@ -227,46 +230,46 @@ class PlotEditor ( Editor ):
                                    plot_bg_color = factory.bg_color,
                                    axis_index    = x_axis,
                                    axis          = y_axis )
-                                   
+
         if factory.title != '':
             pv.add( PlotTitle( text = factory.title ) )
-            
+
         window = Window( parent, component = PlotComponent( component = pv ) )
         self.control = control = window.control
         control.SetSize( ( factory.width, factory.height ) )
-        
+
         for name in factory.traits:
             object, name, value = self.parse_extended_name( name )
             object.on_trait_change( self._update_editor, name,
-                                    dispatch = 'ui' ) 
-        
+                                    dispatch = 'ui' )
+
     #---------------------------------------------------------------------------
-    #  Disposes of the contents of an editor:    
+    #  Disposes of the contents of an editor:
     #---------------------------------------------------------------------------
-                
+
     def dispose ( self ):
         """ Disposes of the contents of an editor.
         """
         super( PlotEditor, self ).dispose()
-        
+
         for name in self.factory.traits:
             object, name, value = self.parse_extended_name( name )
-            object.on_trait_change( self._update_editor, name, 
+            object.on_trait_change( self._update_editor, name,
                                     remove = True )
-            
+
     #---------------------------------------------------------------------------
     #  Updates the editor when the object trait changes external to the editor:
     #---------------------------------------------------------------------------
-        
+
     def update_editor ( self ):
-        """ Updates the editor when the object trait changes externally to the 
+        """ Updates the editor when the object trait changes externally to the
             editor.
         """
         from enthought.chaco.wx.plot import PlotValue
         from numpy import arange
-        
+
         factory  = self.factory
-        
+
         x_values = None
         if factory.x_values != '':
             try:
@@ -277,16 +280,16 @@ class PlotEditor ( Editor ):
                     x_values = getattr( self.object, factory.x_values )
                 except:
                     pass
-                    
+
         if x_values is None:
             try:
                 low, high, step = factory.x_range
                 x_values = arange( low, high + (step / 2.0), step )
             except:
                 x_values = arange( 0.0, 1.005, 0.01 )
-                
+
         try:
-            y_values = getattr( self.ui.handler, factory.y_values )( 
+            y_values = getattr( self.ui.handler, factory.y_values )(
                                 self.ui.info, x_values )
         except:
             try:
@@ -294,6 +297,6 @@ class PlotEditor ( Editor ):
             except:
                 t = x_values - 0.5
                 y_values = t * t
-                
+
         self._pv.set( data = y_values, index = PlotValue( x_values ) )
         self._pv.update()
