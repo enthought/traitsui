2d1f2a873a4cdde0b81c0ca1a7cbfcaaf1de5fb4
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2007-08-21 19:35:07 +0000

    * More setuptools / egg-building infrastructure changes.  Added dependency links, the nonets extra, and support for running setuptools test command.

--- setup.py
+++ setup.py
@@ -1,24 +1,59 @@
 from setuptools import setup, find_packages
 
+
+# Function to convert simple ETS project names and versions to a requirements
+# spec that works for both development builds and stable builds.  Allows
+# a caller to specify a max version, which is intended to work along with
+# Enthought's standard versioning scheme -- see the following write up:
+#    https://svn.enthought.com/enthought/wiki/EnthoughtVersionNumbers
+def etsdep(p, min, max=None, literal=False):
+    require = '%s >=%s.dev' % (p, min)
+    if max is not None:
+        if literal is False:
+            require = '%s, <%s.a' % (require, max)
+        else:
+            require = '%s, <%s' % (require, max)
+    return require
+
+
+# Declare our ETS project dependencies.
+MODEL = etsdep('enthought.model', '2.0.0b1')
+TRAITS = etsdep('enthought.traits', '3.0.0b1')
+
+
 setup(
-    name = 'enthought.traits.ui.wx',
-    version = '3.0.0b1',
-    description  = 'WxPython backend for enthought.traits',
-    author       = 'David C. Morrill',
+    author = 'David C. Morrill',
     author_email = 'dmorrill@enthought.com',
-    url          = 'http://code.enthought.com/traits',
-    license      = 'BSD',
-    zip_safe     = False,
-    packages = find_packages(),
+    dependency_links = [
+        'http://code.enthought.com/enstaller/eggs/source',
+        'http://code.enthought.com/enstaller/eggs/source/unstable',
+        ],
+    description = 'WxPython backend for enthought.traits',
+    extras_require = {
+        # All non-ets dependencies should be in this extra to ensure users can
+        # decide whether to require them or not.
+        'nonets': [
+            #'wx',  # fixme: not available as an egg on all platforms.
+            ],
+        },
     include_package_data = True,
     install_requires = [
-        #'wx',  # fixme: not available as an egg on all platforms.
-        "enthought.model >=2.0.0b1, <3.0.0",
-        "enthought.traits >=3.0.0b1, <4.0.0",
-    ],
+        MODEL,
+        TRAITS,
+        ],
+    license = 'BSD',
+    name = 'enthought.traits.ui.wx',
     namespace_packages = [
         "enthought",
         "enthought.traits",
         "enthought.traits.ui",
-    ],
-)
+        ],
+    packages = find_packages(),
+    tests_require = [
+        'nose >= 0.9',
+        ],
+    test_suite = 'nose.collector',
+    url = 'http://code.enthought.com/traits',
+    version = '3.0.0b1',
+    zip_safe = False,
+    )
