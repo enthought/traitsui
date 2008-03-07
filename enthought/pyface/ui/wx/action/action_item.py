893290cb277a091e8f99a67403eca81cfadf98a9
dmorrill <dmorrill@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-03-07 23:35:39 +0000

    Cleaned up module copyright notices.

--- enthought/pyface/ui/wx/action/action_item.py
+++ enthought/pyface/ui/wx/action/action_item.py
@@ -1,18 +1,21 @@
 #------------------------------------------------------------------------------
-# Copyright (c) 2005, Enthought, Inc.
-# All rights reserved.
 #
-# This software is provided without warranty under the terms of the BSD
-# license included in enthought/LICENSE.txt and may be redistributed only
-# under the conditions described in the aforementioned license.  The license
-# is also available online at http://www.enth373ought.com/licenses/BSD.txt
-# Thanks for using Enthought open source!
+#  Copyright (c) 2005, Enthought, Inc.
+#  All rights reserved.
+#  
+#  This software is provided without warranty under the terms of the BSD
+#  license included in enthought/LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
 #
-# Author: Enthought, Inc.
-# Description: <Enthought pyface package component>
+#  Thanks for using Enthought open source!
+#  
+#  Author: Enthought, Inc.
+#
 #------------------------------------------------------------------------------
-""" The wx specific implementations the action manager internal classes. """
 
+""" The wx specific implementations the action manager internal classes. 
+"""
 
 # Standard libary imports.
 from inspect import getargspec
