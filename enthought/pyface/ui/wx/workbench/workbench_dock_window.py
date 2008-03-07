d20c8c9df7b4de9a18dfa42b7310148e002747ba
dmorrill <dmorrill@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-03-07 23:49:32 +0000

    Cleaned up module copyright notices.

--- enthought/pyface/ui/wx/workbench/workbench_dock_window.py
+++ enthought/pyface/ui/wx/workbench/workbench_dock_window.py
@@ -1,5 +1,21 @@
-""" Base class for workbench dock windows. """
+#------------------------------------------------------------------------------
+#
+#  Copyright (c) 2005, Enthought, Inc.
+#  All rights reserved.
+# 
+#  This software is provided without warranty under the terms of the BSD
+#  license included in enthought/LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+#  Thanks for using Enthought open source!
+# 
+#  Author: Enthought, Inc.
+#
+#------------------------------------------------------------------------------
 
+""" Base class for workbench dock windows.
+"""
 
 # Enthought library imports.
 from enthought.logger.api import logger
