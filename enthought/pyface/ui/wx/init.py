9f2c8738ea08c71cbd0ec8a91ca26339f8f11cff
dmorrill <dmorrill@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-03-07 23:33:18 +0000

    Cleaned up module copyright notices.

--- enthought/pyface/ui/wx/init.py
+++ enthought/pyface/ui/wx/init.py
@@ -1,13 +1,14 @@
 #------------------------------------------------------------------------------
-# Copyright (c) 2007, Riverbank Computing Limited
-# All rights reserved.
-# 
-# This software is provided without warranty under the terms of the BSD
-# license included in enthought/LICENSE.txt and may be redistributed only
-# under the conditions described in the aforementioned license.  The license
-# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+#  Copyright (c) 2007, Riverbank Computing Limited
+#  All rights reserved.
+#  
+#  This software is provided without warranty under the terms of the BSD
+#  license included in enthought/LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
+#
 #------------------------------------------------------------------------------
-
 
 # Major package imports.
 import wx
