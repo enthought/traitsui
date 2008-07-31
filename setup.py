9ce9e00d5f67841215cb2c0cd49495305a8885f3
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-07-31 00:59:45 +0000

    * Bumping version number after releasing.

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ setup(
         ],
     # test_suite = 'nose.collector',
     url = 'http://code.enthought.com/ets',
-    version = '3.0.0',
+    version = '3.0.1',
     zip_safe = False,
     )
 
