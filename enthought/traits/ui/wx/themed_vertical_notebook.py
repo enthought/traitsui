ef883cc1fc8448e777fb705d207d6876e80ccf2f
dmorrill <dmorrill@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2008-08-26 21:46:13 +0000

    Updated default image names.

--- enthought/traits/ui/wx/themed_vertical_notebook.py
+++ enthought/traits/ui/wx/themed_vertical_notebook.py
@@ -282,10 +282,10 @@ class ThemedVerticalNotebook ( HasPrivateTraits ):
     #-- Public Traits ----------------------------------------------------------
 
     # The theme to use for 'closed' notebook pages:
-    closed_theme = ATheme( Theme( '@std:BH5', content = 0 ) )
+    closed_theme = ATheme( Theme( '@std:notebook_close', content = 0 ) )
 
     # The theme to use for 'open' notebook pages:
-    open_theme = ATheme( Theme( 'nb_open', content = 0 ) )
+    open_theme = ATheme( Theme( '@std:notebook_open', content = 0 ) )
 
     # Allow multiple open pages at once?
     multiple_open = Bool( False )
