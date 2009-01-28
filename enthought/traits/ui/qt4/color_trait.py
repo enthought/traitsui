d8b3f6d3bc2c43b5c7ae6386f5aea20171bbc618
GaelVaroquaux <GaelVaroquaux@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-01-28 21:02:29 +0000

    Add warning on the GPL exception of PyQt in the licensing notes

--- enthought/traits/ui/qt4/color_trait.py
+++ enthought/traits/ui/qt4/color_trait.py
@@ -2,8 +2,9 @@
 # Copyright (c) 2007, Riverbank Computing Limited
 # All rights reserved.
 #
-# This software is provided without warranty under the terms of the BSD license
-# license.
+# This software is provided without warranty under the terms of the BSD license.
+# However, when used with the GPL version of PyQt the additional terms described in the PyQt GPL exception also apply
+
 #
 # Author: Riverbank Computing Limited
 #------------------------------------------------------------------------------
