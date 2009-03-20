597087cfe6988f2637ff46828fcff12a49e4fd8a
dpeterson <dpeterson@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-03-20 22:19:30 +0000

    * Smallest possible version number bump after making a release. * Updated dependency versions to match what is currently (or will soon be, after I finish checking everything in) in the trunk of other projects.

--- setup.py
+++ setup.py
@@ -45,11 +45,11 @@ def etsdep(p, min, max=None, literal=False):
 
 
 # Declare our ETS project dependencies.
-APPTOOLS = etsdep('AppTools', '3.2.0')
-ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.2')
-ETSDEVTOOLS_DEVELOPER = etsdep('ETSDevTools[developer]', '3.0.2')
-TRAITS = etsdep('Traits', '3.1.0')
-TRAITSGUI_DOCK = etsdep('TraitsGUI[dock]', '3.0.4')
+APPTOOLS = etsdep('AppTools', '3.2.1')
+ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.3')
+ETSDEVTOOLS_DEVELOPER = etsdep('ETSDevTools[developer]', '3.0.3')
+TRAITS = etsdep('Traits', '3.1.1')
+TRAITSGUI_DOCK = etsdep('TraitsGUI[dock]', '3.0.5')
 
 
 # The following soft dependencies are handled with appropriate try...except
@@ -125,7 +125,7 @@ setup(
         ],
     # test_suite = 'nose.collector',
     url = 'http://code.enthought.com/projects/traits_gui',
-    version = '3.1.0',
+    version = '3.1.1',
     zip_safe = False,
     )
 
