71e51f8f4adf23e4fd581c7334da9e269ae7f6c2
bhendrix <bhendrix@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2009-04-22 00:17:48 +0000

    Changed the dialog bg color to use the menubar color which is a better guess at the system window color than the button face

--- enthought/pyface/ui/wx/system_metrics.py
+++ enthought/pyface/ui/wx/system_metrics.py
@@ -2,14 +2,14 @@
 #
 #  Copyright (c) 2005, Enthought, Inc.
 #  All rights reserved.
-#  
+#
 #  This software is provided without warranty under the terms of the BSD
 #  license included in enthought/LICENSE.txt and may be redistributed only
 #  under the conditions described in the aforementioned license.  The license
 #  is also available online at http://www.enthought.com/licenses/BSD.txt
 #
 #  Thanks for using Enthought open source!
-#  
+#
 #  Author: Enthought, Inc.
 #
 #------------------------------------------------------------------------------
@@ -60,7 +60,7 @@ class SystemMetrics(MSystemMetrics, HasTraits):
             # wx lies.
             color = wx.Colour(232, 232, 232)
         else:
-            color = wx.SystemSettings_GetColour(wx.SYS_COLOUR_BTNFACE).Get()
+            color = wx.SystemSettings_GetColour(wx.SYS_COLOUR_MENUBAR).Get()
 
         return (color[0]/255., color[1]/255., color[2]/255.)
 
