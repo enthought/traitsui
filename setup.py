1ff6ea46b69d496a3e118a2a68d019648e6c3c3c
pwang <pwang@651a555e-23ca-0310-84fe-ca9f7c59d2ea>
2010-02-21 14:41:46 +0000

    separating out setup_data.py from setup.py in TraitsBackendWX

--- setup.py
+++ setup.py
@@ -24,38 +24,16 @@ TraitsBackendWX:
 
 from setuptools import setup, find_packages
 
+# FIXME: This works around a setuptools bug which gets setup_data.py metadata
+# from incorrect packages. Ticket #1592
+#from setup_data import INFO
+setup_data = dict(__name__='', __file__='setup_data.py')
+execfile('setup_data.py', setup_data)
+INFO = setup_data['INFO']
 
 # Pull the description values for the setup keywords from our file docstring.
 DOCLINES = __doc__.split("\n")
 
-
-# Function to convert simple ETS project names and versions to a requirements
-# spec that works for both development builds and stable builds.  Allows
-# a caller to specify a max version, which is intended to work along with
-# Enthought's standard versioning scheme -- see the following write up:
-#    https://svn.enthought.com/enthought/wiki/EnthoughtVersionNumbers
-def etsdep(p, min, max=None, literal=False):
-    require = '%s >=%s.dev' % (p, min)
-    if max is not None:
-        if literal is False:
-            require = '%s, <%s.a' % (require, max)
-        else:
-            require = '%s, <%s' % (require, max)
-    return require
-
-
-# Declare our ETS project dependencies.
-APPTOOLS = etsdep('AppTools', '3.3.1')
-ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.4')
-ETSDEVTOOLS_DEVELOPER = etsdep('ETSDevTools[developer]', '3.0.4')
-TRAITS = etsdep('Traits', '3.2.1')
-TRAITSGUI_DOCK = etsdep('TraitsGUI[dock]', '3.1.1')
-
-
-# The following soft dependencies are handled with appropriate try...except
-# wrappers in the code:
-# AppTools -- used in traits.ui.wx.dnd_editor.py
-# ETSDevTools -- used in traits.ui.wx.helper.py and view_application.py
 
 
 setup(
@@ -77,37 +55,14 @@ setup(
         Topic :: Software Development :: Libraries
         """.splitlines() if len(c.split()) > 0],
     description = DOCLINES[1],
-    extras_require = {
-
-        # Extra denoting that complete drag and drop support for files and
-        # named bindings should be installed:
-        'dnd': [
-            APPTOOLS,
-            ],
-
-        # Extra denoting that complete developer debug support for the ETS FBI
-        # debugger should be installed:
-        'debug': [
-            ETSDEVTOOLS_DEVELOPER,
-            ],
-
-        # All non-ets dependencies should be in this extra to ensure users can
-        # decide whether to require them or not.
-        'nonets': [
-            #'wx',  # fixme: not available as an egg on all platforms.
-            ],
-        },
+    extras_require = INFO['extras_require'],
     include_package_data = True,
-    install_requires = [
-        ENTHOUGHTBASE_UI,
-        TRAITSGUI_DOCK,
-        TRAITS,
-        ],
+    install_requires = INFO['install_requires'],
     license = 'BSD',
     long_description = '\n'.join(DOCLINES[3:]),
     maintainer = 'ETS Developers',
     maintainer_email = 'enthought-dev@enthought.com',
-    name = 'TraitsBackendWX',
+    name = INFO['name'], 
     namespace_packages = [
         'enthought',
         'enthought.pyface',
@@ -125,7 +80,7 @@ setup(
         ],
     # test_suite = 'nose.collector',
     url = 'http://code.enthought.com/projects/traits_gui',
-    version = '3.2.1',
+    version = INFO['version'],
     zip_safe = False,
     )
 
---
+++ setup_data.py
@@ -0,0 +1,62 @@
+
+# Function to convert simple ETS project names and versions to a requirements
+# spec that works for both development builds and stable builds.  Allows
+# a caller to specify a max version, which is intended to work along with
+# Enthought's standard versioning scheme -- see the following write up:
+#    https://svn.enthought.com/enthought/wiki/EnthoughtVersionNumbers
+def etsdep(p, min, max=None, literal=False):
+    require = '%s >=%s.dev' % (p, min)
+    if max is not None:
+        if literal is False:
+            require = '%s, <%s.a' % (require, max)
+        else:
+            require = '%s, <%s' % (require, max)
+    return require
+
+
+# Declare our ETS project dependencies.
+APPTOOLS = etsdep('AppTools', '3.3.1')
+ENTHOUGHTBASE_UI = etsdep('EnthoughtBase[ui]', '3.0.4')
+ETSDEVTOOLS_DEVELOPER = etsdep('ETSDevTools[developer]', '3.0.4')
+TRAITS = etsdep('Traits', '3.2.1')
+TRAITSGUI_DOCK = etsdep('TraitsGUI[dock]', '3.1.1')
+
+# The following soft dependencies are handled with appropriate try...except
+# wrappers in the code:
+# AppTools -- used in traits.ui.wx.dnd_editor.py
+# ETSDevTools -- used in traits.ui.wx.helper.py and view_application.py
+
+
+# A dictionary of the setup data information.
+INFO = {
+
+    'extras_require' : {
+
+        # Extra denoting that complete drag and drop support for files and
+        # named bindings should be installed:
+        'dnd': [
+            APPTOOLS,
+            ],
+
+        # Extra denoting that complete developer debug support for the ETS FBI
+        # debugger should be installed:
+        'debug': [
+            ETSDEVTOOLS_DEVELOPER,
+            ],
+
+        # All non-ets dependencies should be in this extra to ensure users can
+        # decide whether to require them or not.
+        'nonets': [
+            #'wx',  # fixme: not available as an egg on all platforms.
+            ],
+        },
+    'install_requires' : [
+        ENTHOUGHTBASE_UI,
+        TRAITSGUI_DOCK,
+        TRAITS,
+        ],
+    'name': 'TraitsBackendWX',
+    'version': '3.2.1',
+    }
+
+
