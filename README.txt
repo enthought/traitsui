70c343435f1350dfbf8f0d99cc6636f1768a49bd
Ilan Schnell <ischnell@enthought.com>
2011-04-20 16:43:46 -0500

    prepare traitsbackendqt for merge

--- README.txt
+++
@@ -1,2 +0,0 @@
-The TraitsBackendQt project, a part of the Enthought Tool Suite.
-
--- setup_data.py
+++
@@ -1,7 +0,0 @@
-INFO = {
-    'name': 'TraitsBackendQt',
-    'version': '3.6.1',
-    'install_requires': [
-        'TraitsGUI >= 3.6.0.dev',
-    ],
-}
