647e51dccbf619e51ee28789b69d949e6ccb805e
Pietro Berkes <pietro.berkes@googlemail.com>
2011-11-25 09:24:34 +0000

    TST+BUG: add test for bug in range editor with spinner mode
    The test fails for wx on Mac OS X:
    
    Typing in a value in the text box part of the controller, and pressing
    "OK" without de-focusing raises an exception:
    AttributeError: 'NoneType' object has no attribute 'GetValue'
    
    To run the test manually:
    ETS_TOOLKIT='wx' python test_range_editor_spinner.py
    
    To run the test automatically:
    ETS_TOOLKIT='wx' nosetests test_range_editor_spinner.py

---
+++ traitsui/tests/test_range_editor_spinner.py
@@ -0,0 +1,145 @@
+from contextlib import contextmanager
+from traits.has_traits import HasTraits
+from traits.trait_types import Int
+from traitsui.item import Item
+from traitsui.view import View
+from traitsui.editors.range_editor import RangeEditor
+
+from functools import partial
+import wx
+import nose
+
+
+class NumberWithSpinnerEditor(HasTraits):
+    """Dialog containing a RangeEditor in 'spinner' mode for an Int.
+    """
+
+    number = Int
+
+    traits_view = View(
+        Item('number', editor=RangeEditor(low=3, high=8, mode='spinner')),
+        buttons = ['OK']
+    )
+
+
+@contextmanager
+def capture_exceptions_on_all_threads():
+    """Context manager that captures all exceptions, even those coming from
+    the UI thread. On exit, the first exception is raised (if any).
+    """
+
+    import sys
+    import traceback
+
+    exceptions = []
+
+    def excepthook(type, value, tb):
+        exceptions.append(value)
+        message = 'Uncaught exception:\n'
+        message += ''.join(traceback.format_exception(type, value, tb))
+        print message
+
+    try:
+        sys.excepthook = excepthook
+        yield
+    finally:
+        if len(exceptions) > 0:
+            raise exceptions[0]
+        sys.excepthook = sys.__excepthook__
+
+
+def skip_if_not_backend(test_func, backend_name=''):
+    """Decorator that skip tests if the backend is not the desired one."""
+    from traits.etsconfig.api import ETSConfig
+    if ETSConfig.toolkit != backend_name:
+        def test_func():
+            raise nose.SkipTest
+    return test_func
+
+skip_if_not_wx = partial(skip_if_not_backend, backend_name='wx')
+skip_if_not_qt4 = partial(skip_if_not_backend, backend_name='qt4')
+
+
+@skip_if_not_wx
+def test_wx_spin_control_editing():
+    # behavior: when editing the text part of a spin control box, pressing
+    # the OK button should update the value of the HasTraits class
+    # (tests a bug where this fails with an AttributeError
+
+    with capture_exceptions_on_all_threads():
+        num = NumberWithSpinnerEditor()
+        ui = num.edit_traits()
+
+        # the following is equivalent to clicking in the text control of the
+        # range editor, enter a number, and clicking ok without defocusing
+
+        # the frame
+        panel = ui.control.GetChildren()[0]
+
+        # SpinCtrl object
+        spin = panel.GetChildren()[1]
+        spin.SetFocusFromKbd()
+
+        # TextCtrl object of the spin control
+        spintxt = spin.GetChildren()[0]
+        # unlike spintxt.SetValue, this method does not fire a
+        # wxEVT_COMMAND_TEXT_UPDATED event
+        spintxt.ChangeValue('4')
+
+        # make sure that the change in text has not been notified
+        assert spin.GetValue() == 3
+
+        # press the OK button and close the dialog
+        okbutton = ui.control.GetChildren()[2]
+        click_event = wx.CommandEvent(wx.wxEVT_COMMAND_BUTTON_CLICKED,
+                                      okbutton.GetId())
+        okbutton.ProcessEvent(click_event)
+
+    # if all went well, the number traits has been updated and its value is 4
+    assert num.number == 4
+
+
+@skip_if_not_qt4
+def test_qt_spin_control_editing():
+    # behavior: when editing the text part of a spin control box, pressing
+    # the OK button should update the value of the HasTraits class
+
+    with capture_exceptions_on_all_threads():
+        num = NumberWithSpinnerEditor()
+        ui = num.edit_traits()
+
+        # the following is equivalent to clicking in the text control of the
+        # range editor, enter a number, and clicking ok without defocusing
+
+        mainwin = ui.control.children()[1]
+        widget = mainwin.children()[2]
+
+        # spin control
+        spin = widget.children()[2]
+
+        # text element inside the spin control
+        lineedit = spin.children()[0]
+        lineedit.setFocus()
+
+        #keyevent =qt.QtGui.QKeyEvent(qt.QtCore.QEvent.KeyPress,
+        #                             qt.QtCore.Qt.Key_4,
+        #                             qt.QtCore.Qt.NoModifier)
+        #app = qt.QtGui.QApplication.instance()
+        #app.sendEvent(lineedit, keyevent)
+
+        # NOTE: I'm not sure at the moment that this is the exact equivalent
+        # of the wx test
+        lineedit.setText('4')
+
+        # press the OK button and close the dialog
+        bbox = ui.control.children()[2]
+        okb = bbox.children()[1]
+        okb.click()
+
+    # if all went well, the number traits has been updated and its value is 4
+    assert num.number == 4
+
+
+if __name__ == '__main__':
+    num = NumberWithSpinnerEditor()
+    num.configure_traits()
