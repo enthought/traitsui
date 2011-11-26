82b79983050e24e63d278e04c98341d826134c99
Pietro Berkes <pietro.berkes@googlemail.com>
2011-11-26 05:07:18 +0000

    ENH: a couple of debug tools for wx

--- traitsui/tests/_tools.py
+++ traitsui/tests/_tools.py
@@ -6,6 +6,8 @@ import sys
 import traceback
 
 
+# ######### Testing tools
+
 @contextmanager
 def store_exceptions_on_all_threads():
     """Context manager that captures all exceptions, even those coming from
@@ -43,5 +45,47 @@ def skip_if_not_backend(test_func, backend_name=''):
     return test_func
 
 
+#: Test decorator: Skip test if backend is not 'wx'
 skip_if_not_wx = partial(skip_if_not_backend, backend_name='wx')
+
+#: Test decorator: Skip test if backend is not 'qt4'
 skip_if_not_qt4 = partial(skip_if_not_backend, backend_name='qt4')
+
+
+# ######### Debug tools
+
+def wx_apply_on_children(func, node, _level=0):
+    """Print the result of applying a function on `node` and its children.
+    """
+    print '-'*_level + str(node)
+    print ' '*_level + str(func(node)) + '\n'
+    for child in node.GetChildren():
+        wx_apply_on_children(func, child, _level+1)
+
+
+def wx_print_names(node):
+    """Print the name of `node` and its children.
+    """
+    wx_apply_on_children(lambda n: n.GetName(), node)
+
+
+def wx_announce_when_destroyed(node):
+    """Prints a message when `node` is destroyed.
+
+    Use as:
+
+       >>> ui = xxx.edit_traits()
+       >>> wx_apply_on_children(wx_announce_when_destroyed, ui.control)
+    """
+
+    _destroy_method = node.Destroy
+
+    def destroy_wrapped():
+        print 'Destroying:', node
+        #print 'Stack is'
+        #traceback.print_stack()
+        _destroy_method()
+        print 'Destroyed:', node
+
+    node.Destroy = destroy_wrapped
+    return 'Node {} decorated'.format(node.GetName())
