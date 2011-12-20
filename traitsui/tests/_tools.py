4353a132297652afdc9197b192e20bef3fc3b790
Pietro Berkes <pietro.berkes@googlemail.com>
2011-12-20 22:55:41 +0000

    TST: characterization tests for UI.reset, and a failing test for UI.finish
    when a dialog is closed, its children are not destroyed

--- traitsui/tests/_tools.py
+++ traitsui/tests/_tools.py
@@ -5,6 +5,7 @@ import nose
 import sys
 import traceback
 
+from traits.etsconfig.api import ETSConfig
 
 # ######### Testing tools
 
@@ -33,7 +34,6 @@ def store_exceptions_on_all_threads():
 
 def skip_if_not_backend(test_func, backend_name=''):
     """Decorator that skip tests if the backend is not the desired one."""
-    from traits.etsconfig.api import ETSConfig
 
     if ETSConfig.toolkit != backend_name:
         # preserve original name so that it appears in the report
@@ -52,21 +52,51 @@ skip_if_not_wx = partial(skip_if_not_backend, backend_name='wx')
 skip_if_not_qt4 = partial(skip_if_not_backend, backend_name='qt4')
 
 
+def count_calls(func):
+    """Decorator that stores the number of times a function is called.
+
+    The counter is stored in func._n_counts.
+    """
+
+    def wrapped(*args, **kwargs):
+        wrapped._n_calls += 1
+        return func(*args, **kwargs)
+
+    wrapped._n_calls = 0
+
+    return wrapped
+
+
+# ######### Utility tools to test on both qt4 and wx
+
+def get_children(node):
+    if ETSConfig.toolkit == 'wx':
+        return node.GetChildren()
+    else:
+        return node.children()
+
+
 # ######### Debug tools
 
-def wx_apply_on_children(func, node, _level=0):
+def apply_on_children(func, node, _level=0):
     """Print the result of applying a function on `node` and its children.
     """
     print '-'*_level + str(node)
     print ' '*_level + str(func(node)) + '\n'
-    for child in node.GetChildren():
-        wx_apply_on_children(func, child, _level+1)
+    for child in get_children(node):
+        apply_on_children(func, child, _level+1)
 
 
 def wx_print_names(node):
+    """Print the name and id of `node` and its children.
+    """
+    apply_on_children(lambda n: (n.GetName(), n.GetId()), node)
+
+
+def qt_print_names(node):
     """Print the name of `node` and its children.
     """
-    wx_apply_on_children(lambda n: n.GetName(), node)
+    apply_on_children(lambda n: n.objectName(), node)
 
 
 def wx_announce_when_destroyed(node):
@@ -74,8 +104,8 @@ def wx_announce_when_destroyed(node):
 
     Use as:
 
-       >>> ui = xxx.edit_traits()
-       >>> wx_apply_on_children(wx_announce_when_destroyed, ui.control)
+        >>> ui = xxx.edit_traits()
+        >>> apply_on_children(wx_announce_when_destroyed, ui.control)
     """
 
     _destroy_method = node.Destroy
@@ -89,3 +119,19 @@ def wx_announce_when_destroyed(node):
 
     node.Destroy = destroy_wrapped
     return 'Node {} decorated'.format(node.GetName())
+
+
+def wx_find_event_by_number(evt_num):
+    """Find all wx event names that correspond to a ceratin event number.
+
+    Example:
+
+        >>> wx_find_event_by_number(10010)
+        ['wxEVT_COMMAND_MENU_SELECTED', 'wxEVT_COMMAND_TOOL_CLICKED']
+    """
+
+    import wx
+    possible = [attr for attr in dir(wx)
+                if attr.startswith('wxEVT') and getattr(wx, attr) == evt_num]
+
+    return possible
