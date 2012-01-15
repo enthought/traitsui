0b2c0de13d7d0b02eef2577d7450177c4c9f7197
itziakos <ioannist@enthought.com>
2012-01-15 22:54:56 +0000

    FIX: fix issue with run-away change notification in the CSVListEditor

---
+++ traitsui/qt4/csv_list_editor.py
@@ -0,0 +1,46 @@
+#------------------------------------------------------------------------------
+#
+#  Copyright (c) 2005, Enthought, Inc.
+#  All rights reserved.
+#
+#  This software is provided without warranty under the terms of the BSD
+#  license included in enthought/LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+#  Thanks for using Enthought open source!
+#
+#  Author: Ioannis Tziakos
+#  Date:   11 Jan 2012
+#
+#------------------------------------------------------------------------------
+
+""" Defines the various text editors for the Qt user interface toolkit.
+    The module is mainly a place-folder for TextEditor factories that have
+    been augmented to also listen to changes in the items of the list object.
+"""
+
+#-------------------------------------------------------------------------------
+#  Imports:
+#------------------------------------------------------------------------------
+
+from .text_editor import (SimpleEditor, CustomEditor,
+                         ReadonlyEditor, TextEditor)
+from ..editors.csv_list_editor import hook_on_item_change
+
+class SimpleEditor(WXSimpleEditor):
+    """ Simple Editor style for CSVListEditor. """
+    prepare = _prepare_method
+    dispose = _dispose_method
+
+class CustomEditor(WXCustomEditor):
+    """ Custom Editor style for CSVListEditor. """
+    prepare = _prepare_method
+    dispose = _dispose_method
+
+class ReadonlyEditor(WXReadonlyEditor):
+    """ Readonly Editor style for CSVListEditor. """
+    prepare = _prepare_method
+    dispose = _dispose_method
+
+TextEditor = SimpleEditor
