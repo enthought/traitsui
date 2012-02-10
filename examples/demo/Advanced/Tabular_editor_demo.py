06d50a406bd36c985f43209876c895e3ed0066c5
Jonathan March <jmarch@enthought.com>
2012-02-10 12:00:00 -0600

    Revert "DEMO: Tweak Tabular editor demo to show row titles in QT"
    This reverts commit ae017458d5c0e8eb7ed054b4b935d57418b95c81.
      1. The commit was mis-named. It should have been
      "... to show auto-sizing of columns in QT"
    And
      2. Column Auto-sizing was reverted in
      commit f2421b147bc6440e52914a33722a938d656ca298,
      so demoing it is no longer possible (yet).

--- examples/demo/Advanced/Tabular_editor_demo.py
+++ examples/demo/Advanced/Tabular_editor_demo.py
@@ -31,7 +31,7 @@ In addition:
    blue.
    
  - If this demo is running under QT, it displays each person's surname
-   in a row label, and auto-sizes the column widths.
+   in a row label.
 
 This example demonstrates:
 
@@ -105,12 +105,8 @@ class ReportAdapter(TabularAdapter):
 tabular_editor = TabularEditor(
     adapter    = ReportAdapter(),
     operations = [ 'move' ],
-    
-    # Row titles and column auto resize are only supported in QT:
-    **(dict(show_row_titles = True, auto_resize = True) 
-       if ETSConfig.toolkit == 'qt4' 
-       else {}
-       )
+    # Row titles are not supported in WX:
+    show_row_titles = ETSConfig.toolkit == 'qt4'
 )
 
 #-- Report Class Definition ----------------------------------------------------
