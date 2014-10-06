7e7434d36862142180bec0bb966e69d08a7a57a3
Ioannis Tziakos <ioannist@enthought.com>
2014-10-06 23:20:08 +0100

    update TupleEditor to support BaseTuples and extra validation (including the new ValidatedTuple)

--- traitsui/editors/tuple_editor.py
+++ traitsui/editors/tuple_editor.py
@@ -16,15 +16,16 @@
 """ Defines the tuple editor factory for all traits user interface toolkits.
 """
 
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 #  Imports:
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
 from __future__ import absolute_import
 
 from traits.trait_base import SequenceTypes, enumerate
 
-from traits.api import Bool, HasTraits, List, Tuple, Unicode, Int, Any, TraitType
+from traits.api import (
+    Bool, Callable, HasTraits, List, BaseTuple, Unicode, Int, Any, TraitType)
 
 # CIRCULAR IMPORT FIXME: Importing from the source rather than traits.ui.api
 # to avoid circular imports, as this EditorFactory will be part of
@@ -39,95 +40,105 @@ from ..editor_factory import EditorFactory
 
 from ..editor import Editor
 
-#-------------------------------------------------------------------------------
-#  'ToolkitEditorFactory' class:
-#-------------------------------------------------------------------------------
 
-class ToolkitEditorFactory ( EditorFactory ):
+#------------------------------------------------------------------------------
+#  'ToolkitEditorFactory' class:
+#------------------------------------------------------------------------------
+
+class ToolkitEditorFactory(EditorFactory):
     """ Editor factory for tuple editors.
     """
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
     #  Trait definitions:
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
 
     # Trait definitions for each tuple field
     types = Any
 
     # Labels for each of the tuple fields
-    labels = List( Unicode )
+    labels = List(Unicode)
 
     # Editors for each of the tuple fields:
-    editors = List( EditorFactory )
+    editors = List(EditorFactory)
 
     # Number of tuple fields or rows
-    cols   = Int( 1 )
+    cols = Int(1)
 
     # Is user input set on every keystroke? This is applied to every field
     # of the tuple, provided the field does not already have an 'auto_set'
     # metadata or an editor defined.
-    auto_set = Bool( True )
+    auto_set = Bool(True)
 
     # Is user input set when the Enter key is pressed? This is applied to
     # every field of the tuple, provided the field does not already have an
     # 'enter_set' metadata or an editor defined.
-    enter_set = Bool( False )
+    enter_set = Bool(False)
 
-#-------------------------------------------------------------------------------
+    # The validation function to use for the Tuple. This will override the
+    # validation function used when the editable Trait is a ValidatedTuple.
+    validation = Callable
+
+
+#------------------------------------------------------------------------------
 #  'SimpleEditor' class:
-#-------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 
-class SimpleEditor ( Editor ):
+class SimpleEditor(Editor):
     """ Simple style of editor for tuples.
 
     The editor displays an editor for each of the fields in the tuple, based on
     the type of each field.
     """
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
     #  Finishes initializing the editor by creating the underlying toolkit
     #  widget:
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
 
-    def init ( self, parent ):
+    def init(self, parent):
         """ Finishes initializing the editor by creating the underlying toolkit
             widget.
         """
-        self._ts     = ts = TupleStructure( self )
-        self._ui     = ui = ts.view.ui( ts, parent, kind = 'subpanel' ).set(
-                                        parent = self.ui )
+        self._ts = ts = TupleStructure(self)
+        self._ui = ui = ts.view.ui(
+            ts, parent, kind='subpanel').set(parent=self.ui)
         self.control = ui.control
         self.set_tooltip()
 
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
     #  Updates the editor when the object trait changes external to the editor:
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
 
-    def update_editor ( self ):
+    def update_editor(self):
         """ Updates the editor when the object trait changes external to the
             editor.
         """
         ts = self._ts
-        for i, value in enumerate( self.value ):
-            setattr( ts, 'f%d' % i, value )
 
-    #---------------------------------------------------------------------------
+        for i, value in enumerate(self.value):
+            setattr(ts, 'f{0}'.format(i), value)
+            if ts.validation is not None:
+                setattr(ts, 'invalid{0}'.format(i), False)
+
+    #--------------------------------------------------------------------------
     #  Returns the editor's control for indicating error status:
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
 
-    def get_error_control ( self ):
+    def get_error_control(self):
         """ Returns the editor's control for indicating error status.
         """
         return self._ui.get_error_controls()
 
-#-------------------------------------------------------------------------------
-#  'TupleStructure' class:
-#-------------------------------------------------------------------------------
 
-class TupleStructure ( HasTraits ):
+#------------------------------------------------------------------------------
+#  'TupleStructure' class:
+#------------------------------------------------------------------------------
+
+class TupleStructure (HasTraits):
     """ Creates a view containing items for each field in a tuple.
     """
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
     #  Trait definitions:
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
 
     # Editor this structure is linked to
     editor = Any
@@ -138,49 +149,60 @@ class TupleStructure ( HasTraits ):
     # Number of tuple fields
     fields = Int
 
-    #---------------------------------------------------------------------------
-    #  Initializes the object:
-    #---------------------------------------------------------------------------
+    # The validation function to use for the Tuple.
+    validation = Callable
 
-    def __init__ ( self, editor ):
+    #--------------------------------------------------------------------------
+    #  Initializes the object:
+    #--------------------------------------------------------------------------
+
+    def __init__(self, editor):
         """ Initializes the object.
         """
         factory = editor.factory
-        types   = factory.types
-        labels  = factory.labels
+        types = factory.types
+        labels = factory.labels
         editors = factory.editors
-        cols    = factory.cols
+        cols = factory.cols
+        validation = factory.validation
 
         # Save the reference to the editor:
         self.editor = editor
 
-        # Get the tuple we are mirroring:
+        # Get the tuple we are mirroring.
         object = editor.value
 
         # For each tuple field, add a trait with the appropriate trait
         # definition and default value:
-        content     = []
-        self.fields = len( object )
-        len_labels  = len( labels )
-        len_editors = len( editors )
+        content = []
+        self.fields = len(object)
+        len_labels = len(labels)
+        len_editors = len(editors)
 
+        # Get global validation function.
+        type = editor.value_trait.handler
+        if hasattr(type, 'validation') and validation is None:
+            self.validation = validation = type.validation
+
+        # Get field types.
         if types is None:
-            type = editor.value_trait.handler
-            if isinstance( type, Tuple ):
+            if isinstance(type, BaseTuple):
                 types = type.types
 
-        if not isinstance( types, SequenceTypes ):
-            types = [ types ]
+        if not isinstance(types, SequenceTypes):
+            types = [types]
 
-        len_types = len( types )
+        len_types = len(types)
         if len_types == 0:
-            types     = [ Any ]
+            types = [Any]
             len_types = 1
 
-        for i, value in enumerate( object ):
-            type = types[ i % len_types ]
+        for i, value in enumerate(object):
+            type = types[i % len_types]
 
             auto_set = enter_set = None
+            # XXX: Should the trait auto_set and enter_set value override
+            #      the user option defined in the factory?
             if isinstance(type, TraitType):
                 auto_set = type.auto_set
                 enter_set = type.enter_set
@@ -197,40 +219,52 @@ class TupleStructure ( HasTraits ):
             if i < len_editors:
                 field_editor = editors[i]
 
-            name = 'f%d' % i
-            self.add_trait( name, type( value, event = 'field',
-                                               auto_set = auto_set,
-                                               enter_set = enter_set ) )
-            item = Item( name = name, label = label, editor = field_editor )
+            name = 'f{0}'.format(i)
+            self.add_trait(name, type(
+                value, event='field', auto_set=auto_set, enter_set=enter_set))
+            if validation is not None:
+                invalid = 'invalid{0}'.format(i)
+                self.add_trait(invalid, Bool)
+            else:
+                invalid = ''
+
+            item = Item(
+                name, label=label, editor=field_editor, invalid=invalid)
             if cols <= 1:
-                content.append( item )
+                content.append(item)
             else:
                 if (i % cols) == 0:
-                    group = Group( orientation = 'horizontal' )
-                    content.append( group )
+                    group = Group(orientation='horizontal')
+                    content.append(group)
 
-                group.content.append( item )
+                group.content.append(item)
 
-        self.view = View( Group( show_labels = (len_labels != 0), *content ) )
+        self.view = View(Group(show_labels=(len_labels != 0), *content))
 
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
     #  Updates the underlying tuple when any field changes value:
-    #---------------------------------------------------------------------------
+    #--------------------------------------------------------------------------
 
-    def _field_changed ( self, name, old, new ):
+    def _field_changed(self, name, old, new):
         """ Updates the underlying tuple when any field changes value.
         """
-        index = int( name[ 1: ] )
+        editor = self.editor
+        value = editor.value
+        index = int(name[1:])
         value = self.editor.value
-        if new != value[ index ]:
-            self.editor.value = tuple( 
-                [ getattr( self, 'f%d' % i ) for i in range( self.fields ) ] )
+        if new != value[index]:
+            new_value = tuple(
+                getattr(self, 'f{0}'.format(i)) for i in range(self.fields))
+            if self.validation is not None:
+                if self.validation(new_value):
+                    editor.value = new_value
+                    for i in range(self.fields):
+                        setattr(self, 'invalid{0}'.format(i), False)
+                else:
+                    setattr(self, 'invalid{0}'.format(index), True)
+            else:
+                editor.value = new_value
 
 
 # Define the TupleEditor class.
 TupleEditor = ToolkitEditorFactory
-
-### EOF #######################################################################
-
-
-
--- traitsui/tests/test_tuple_editor.py
+++ traitsui/tests/test_tuple_editor.py
@@ -18,34 +18,160 @@ Test cases for the TupleEditor object.
 
 import unittest
 
-from traits.api import Float, HasStrictTraits, Str, Tuple
+from traits.api import Float, Int, HasStrictTraits, Str, Tuple, ValidatedTuple
 from traits.testing.api import UnittestTools
 
-from traitsui.api import Item, TupleEditor, View
+from traitsui.tests._tools import dispose_ui, get_traitsui_editor
 
 
 class DummyModel(HasStrictTraits):
-    """ Dummy model with a Tuple trait.
-    """
+
+    value_range = ValidatedTuple(
+        Int(0), Int(1), validation=lambda x: x[0] < x[1])
 
     data = Tuple(Float, Float, Str)
-
-    traits_view = View(Item(name='data', editor=TupleEditor()))
 
 
 class TestTupleEditor(UnittestTools, unittest.TestCase):
 
+    def setUp(self):
+        from traitsui.api import TupleEditor
+        self.tuple_editor = TupleEditor
+
     def test_value_update(self):
         # Regression test for #179
-        model = DummyModel()
-        try:
-            ui = model.edit_traits()
-            with self.assertTraitChanges(model, 'data', count=1):
-                model.data = (3, 4.6, 'nono')
-        finally:
-            if ui is not None:
-                ui.dispose()
 
+        dummy_model = DummyModel()
+        with dispose_ui(dummy_model.edit_traits):
+            with self.assertTraitChanges(dummy_model, 'data', count=1):
+                dummy_model.data = (3, 4.6, 'nono')
 
-if __name__ == '__main__':
-    unittest.run()
+    def test_ui_creation(self):
+        dummy_model = DummyModel()
+        with dispose_ui(dummy_model.edit_traits) as ui:
+            editor = get_traitsui_editor(ui, 'value_range')
+            self.assertIsInstance(editor.factory, self.tuple_editor)
+            self.assertEqual(editor.value, (0, 1))
+            self.assertEqual(
+                editor._ts.trait_get(['f0', 'f1', 'invalid0', 'invalid1']),
+                {'f0': 0, 'f1': 1, 'invalid0': False, 'invalid1': False})
+
+            editor = get_traitsui_editor(ui, 'data')
+            self.assertIsInstance(editor.factory, self.tuple_editor)
+            self.assertEqual(editor.value, (0.0, 0.0, ''))
+            self.assertEqual(
+                editor._ts.trait_get(
+                    ['f0', 'f1', 'f2', 'invalid0', 'invalid1', 'invalid2']),
+                {'f0': 0.0, 'f1': 0.0, 'f2': ''})
+
+    def test_ui_invalid_due_to_failing_custom_validate(self):
+        dummy_model = DummyModel()
+        with dispose_ui(dummy_model.edit_traits) as ui:
+            editor = get_traitsui_editor(ui, 'value_range')
+            fields_ui = editor._ui
+            f0_editor = get_traitsui_editor(fields_ui, 'f0')
+            f1_editor = get_traitsui_editor(fields_ui, 'f1')
+
+            f0_editor.value = 5  # 5 < 1 -> invalid
+            self.assertTrue(f0_editor.invalid)
+            self.assertFalse(f1_editor.invalid)
+            self.assertEqual(editor.value, (0, 1))
+
+            f0_editor.value = -3  # -3 < 1 -> valid
+            self.assertFalse(f0_editor.invalid)
+            self.assertFalse(f1_editor.invalid)
+            self.assertEqual(editor.value, (-3, 1))
+
+            f1_editor.value = -4  # -3 < -4 -> invalid
+            self.assertFalse(f0_editor.invalid)
+            self.assertTrue(f1_editor.invalid)
+            self.assertEqual(editor.value, (-3, 1))
+
+            f1_editor.value = 0  # -3 < 0 -> valid
+            self.assertFalse(f0_editor.invalid)
+            self.assertFalse(f1_editor.invalid)
+            self.assertEqual(editor.value, (-3, 0))
+
+    def test_when_editor_is_used_with_vertical_layout(self):
+
+        class VSimple(HasStrictTraits):
+
+            value_range = ValidatedTuple(
+                Int(0), Int(1), cols=1, validation=lambda x: x[0] < x[1])
+
+        dummy_model = VSimple()
+        with dispose_ui(dummy_model.edit_traits) as ui:
+            editor = get_traitsui_editor(ui, 'value_range')
+            self.assertIsInstance(editor.factory, self.tuple_editor)
+            self.assertEqual(editor.value, (0, 1))
+            self.assertEqual(
+                editor._ts.trait_get(['f0', 'f1', 'invalid0', 'invalid1']),
+                {'f0': 0, 'f1': 1, 'invalid0': False, 'invalid1': False})
+
+    def test_when_editor_is_used_with_horizontal_layout(self):
+
+        class HSimple(HasStrictTraits):
+
+            value_range = ValidatedTuple(
+                Int(0), Int(1), cols=2, validation=lambda x: x[0] < x[1])
+
+        dummy_model = HSimple()
+        with dispose_ui(dummy_model.edit_traits) as ui:
+            editor = get_traitsui_editor(ui, 'value_range')
+            self.assertIsInstance(editor.factory, self.tuple_editor)
+            self.assertEqual(editor.value, (0, 1))
+            self.assertEqual(
+                editor._ts.trait_get(['f0', 'f1', 'invalid0', 'invalid1']),
+                {'f0': 0, 'f1': 1, 'invalid0': False, 'invalid1': False})
+
+    def test_when_editor_is_used_in_the_view(self):
+        from traitsui.api import Item, View
+
+        class SimpleWithView(HasStrictTraits):
+
+            value_range = ValidatedTuple(
+                Int(0), Int(1), validation=lambda x: x[0] < x[1])
+
+            view = View(
+                Item('value_range', editor=self.tuple_editor())
+            )
+
+        dummy_model = SimpleWithView()
+        with dispose_ui(dummy_model.edit_traits) as ui:
+            editor = get_traitsui_editor(ui, 'value_range')
+            self.assertIsInstance(editor.factory, self.tuple_editor)
+            self.assertEqual(editor.value, (0, 1))
+            self.assertEqual(
+                editor._ts.trait_get(['f0', 'f1', 'invalid0', 'invalid1']),
+                {'f0': 0, 'f1': 1, 'invalid0': False, 'invalid1': False})
+
+            fields_ui = editor._ui
+            f0_editor = get_traitsui_editor(fields_ui, 'f0')
+            f1_editor = get_traitsui_editor(fields_ui, 'f1')
+
+            f1_editor.value = -4  # 0 < -4 -> invalid
+            self.assertFalse(f0_editor.invalid)
+            self.assertTrue(f1_editor.invalid)
+            self.assertEqual(editor.value, (0, 1))
+
+    def test_invalid_state_reset_on_model_change(self):
+        dummy_model = DummyModel()
+        with dispose_ui(dummy_model.edit_traits) as ui:
+            editor = get_traitsui_editor(ui, 'value_range')
+            fields_ui = editor._ui
+            f0_editor = get_traitsui_editor(fields_ui, 'f0')
+            f1_editor = get_traitsui_editor(fields_ui, 'f1')
+
+            # given
+            f1_editor.value = -4  # 0 < -4 -> invalid
+            self.assertFalse(f0_editor.invalid)
+            self.assertTrue(f1_editor.invalid)
+            self.assertEqual(editor.value, (0, 1))
+
+            # when
+            dummy_model.value_range = (2, 7)
+
+            # then
+            self.assertEqual(
+                editor._ts.trait_get(['f0', 'f1', 'invalid0', 'invalid1']),
+                {'f0': 2, 'f1': 7, 'invalid0': False, 'invalid1': False})
