8c731fa7e25f4bf2f1fd5f1d84003288f135fea8
Ioannis Tziakos <ioannist@enthought.com>
2015-05-22 18:13:23 +0100

    fix #206

--- setup.py
+++ setup.py
@@ -69,14 +69,14 @@ if not is_released:
     elif os.path.exists('traitsui/_version.py'):
         # must be a source distribution, use existing version file
         try:
-            from traits._version import git_revision as git_rev
-            from traits._version import full_version as full_v
+            from traitsui._version import git_revision
+            from traitsui._version import full_version
         except ImportError:
             raise ImportError("Unable to import git_revision. Try removing "
                               "traitsui/_version.py and the build directory "
                               "before building.")
 
-        match = re.match(r'.*?\.dev(?P<dev_num>\d+)', full_v)
+        match = re.match(r'.*?\.dev(?P<dev_num>\d+)', full_version)
         if match is None:
             dev_num = '0'
         else:
@@ -91,7 +91,7 @@ if not is_released:
     with open(filename, "wt") as fp:
         fp.write(template.format(version=VERSION,
                                  full_version=fullversion,
-                                 git_revision=git_rev,
+                                 git_revision=git_revision,
                                  is_released=IS_RELEASED))
 
 if __name__ == "__main__":
