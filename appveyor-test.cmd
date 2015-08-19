55538c9f42460fc8a2ad8b24419897167bab99ce
Ioannis Tziakos <ioannist@enthought.com>
2015-08-19 15:37:36 +0100

    run the qt specific tests on all setups

--- appveyor-test.cmd
+++ appveyor-test.cmd
@@ -3,5 +3,5 @@ copy .coveragerc testrun/
 cd testrun
 if %errorlevel% neq 0 exit /b %errorlevel%
 coverage run -m nose.core -v traitsui.tests
-IF %PYTHON% EQU "C:/Python34-x64" coverage run -a -m nose.core -v traitsui.qt4.tests
+coverage run -a -m nose.core -v traitsui.qt4.tests
 if %errorlevel% neq 0 exit /b %errorlevel%
