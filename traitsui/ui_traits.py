2079428fe0c42818cc1da5cf6b6e427361252b35
Corran Webster <cwebster@enthought.com>
2016-06-16 20:56:49 -0400

    Move some ui_traits to Pyface.

--- traitsui/ui_traits.py
+++ traitsui/ui_traits.py
@@ -24,10 +24,11 @@
 
 from __future__ import absolute_import
 
-from traits.api import (Any, Delegate, Enum, Expression, Float, HasStrictTraits,
-    Instance, List, Range, Str, Trait, TraitError, TraitPrefixList, TraitType)
+from pyface.ui_traits import (Alignment, Border, HasBorder, HasMargin, Image,
+                             Margin, Position, convert_bitmap, convert_image)
 
-from traits.trait_base import get_resource_path
+from traits.api import (Any, Delegate, Enum, Expression, Float, HasStrictTraits,
+    List, Range, Str, Trait, TraitError, TraitPrefixList, TraitType)
 
 #-------------------------------------------------------------------------------
 #  Trait definitions:
@@ -148,100 +149,6 @@ class ViewStatus ( TraitType ):
 
         self.error( object, name, value )
 
-#-------------------------------------------------------------------------------
-#  'Image' trait:
-#-------------------------------------------------------------------------------
-
-image_resource_cache = {}
-image_bitmap_cache   = {}
-
-def convert_image ( value, level = 3 ):
-    """ Converts a specified value to an ImageResource if possible.
-    """
-    global image_resource_cache
-
-    if not isinstance( value, basestring ):
-        return value
-
-    key             = value
-    is_traits_image = (value[:1] == '@')
-    if not is_traits_image:
-        search_path = get_resource_path( level )
-        key         = '%s[%s]' % ( value, search_path )
-
-    result = image_resource_cache.get( key )
-    if result is None:
-        if is_traits_image:
-            try:
-                from .image.image import ImageLibrary
-
-                result = ImageLibrary.image_resource( value )
-            except:
-                result = None
-        else:
-            from pyface.image_resource import ImageResource
-            result = ImageResource( value, search_path = [ search_path ] )
-
-        image_resource_cache[ key ] = result
-
-    return result
-
-def convert_bitmap ( image_resource ):
-    """ Converts an ImageResource to a bitmap using a cache.
-    """
-    global image_bitmap_cache
-
-    bitmap = image_bitmap_cache.get( image_resource )
-    if (bitmap is None) and (image_resource is not None):
-        #try:
-        image_bitmap_cache[ image_resource ] = bitmap = \
-            image_resource.create_bitmap()
-        #except:
-        #    pass
-
-    return bitmap
-
-class Image ( TraitType ):
-    """ Defines a trait whose value must be a PyFace ImageResource or a string
-        that can be converted to one.
-    """
-
-    # Define the default value for the trait:
-    default_value = None
-
-    # A description of the type of value this trait accepts:
-    info_text = 'an ImageResource or string that can be used to define one'
-
-    def __init__ ( self, value = None, **metadata ):
-        """ Creates an Image trait.
-
-        Parameters
-        ----------
-        value : string or ImageResource
-            The default value for the Image, either an ImageResource object,
-            or a string from which an ImageResource object can be derived.
-        """
-        super( Image, self ).__init__( convert_image( value ), **metadata )
-
-    def validate ( self, object, name, value ):
-        """ Validates that a specified value is valid for this trait.
-        """
-        from pyface.image_resource import ImageResource
-
-        if value is None:
-            return None
-
-        new_value = convert_image( value, 4 )
-        if isinstance( new_value, ImageResource ):
-            return new_value
-
-        self.error( object, name, value )
-
-    def create_editor ( self ):
-        """ Returns the default UI editor for the trait.
-        """
-        from .editors.api import ImageEditor
-        return ImageEditor()
 
 #-------------------------------------------------------------------------------
 #  'ATheme' trait:
@@ -304,154 +211,8 @@ class ATheme ( TraitType ):
         self.error( object, name, value )
 
 #-------------------------------------------------------------------------------
-#  'BasePMB' class:
-#-------------------------------------------------------------------------------
-
-class BaseMB ( HasStrictTraits ):
-
-    def __init__ ( self, *args, **traits ):
-        """ Initializes the object.
-        """
-        n = len( args )
-        if n > 0:
-            if n == 1:
-                left = right = top = bottom = args[0]
-            elif n == 2:
-                left = right  = args[0]
-                top  = bottom = args[1]
-            elif n == 4:
-                left, right, top, bottom = args
-            else:
-                raise TraitError( '0, 1, 2 or 4 arguments expected, but %d '
-                                  'specified' % n )
-            self.set( left = left, right = right, top = top, bottom = bottom )
-
-        super( BaseMB, self ).__init__( **traits )
-
-#-------------------------------------------------------------------------------
-#  'Margin' class:
-#-------------------------------------------------------------------------------
-
-class Margin ( BaseMB ):
-
-    # The amount of padding/margin at the top:
-    top = Range( -32, 32, 0 )
-
-    # The amount of padding/margin at the bottom:
-    bottom = Range( -32, 32, 0 )
-
-    # The amount of padding/margin on the left:
-    left = Range( -32, 32, 0 )
-
-    # The amount of padding/margin on the right:
-    right = Range( -32, 32, 0 )
-
-#-------------------------------------------------------------------------------
-#  'Border' class:
-#-------------------------------------------------------------------------------
-
-class Border ( BaseMB ):
-
-    # The amount of border at the top:
-    top = Range( 0, 32, 0 )
-
-    # The amount of border at the bottom:
-    bottom = Range( 0, 32, 0 )
-
-    # The amount of border on the left:
-    left = Range( 0, 32, 0 )
-
-    # The amount of border on the right:
-    right = Range( 0, 32, 0 )
-
-#-------------------------------------------------------------------------------
-#  'HasMargin' trait:
-#-------------------------------------------------------------------------------
-
-class HasMargin ( TraitType ):
-    """ Defines a trait whose value must be a Margin object or an integer or
-        tuple value that can be converted to one.
-    """
-
-    # The desired value class:
-    klass = Margin
-
-    # Define the default value for the trait:
-    default_value = Margin( 0 )
-
-    # A description of the type of value this trait accepts:
-    info_text = ('a Margin instance, or an integer in the range from -32 to 32 '
-                 'or a tuple with 1, 2 or 4 integers in that range that can be '
-                 'used to define one')
-
-    def validate ( self, object, name, value ):
-        """ Validates that a specified value is valid for this trait.
-        """
-        if isinstance( value, int ):
-            try:
-                value = self.klass( value )
-            except:
-                self.error( object, name, value )
-        elif isinstance( value, tuple ):
-            try:
-                value = self.klass( *value )
-            except:
-                self.error( object, name, value )
-
-        if isinstance( value, self.klass ):
-            return value
-
-        self.error( object, name, value )
-
-    def get_default_value ( self ):
-        """ Returns a tuple of the form: ( default_value_type, default_value )
-            which describes the default value for this trait.
-        """
-        dv  = self.default_value
-        dvt = self.default_value_type
-        if dvt < 0:
-            if isinstance( dv, int ):
-                dv = self.klass( dv )
-            elif isinstance( dv, tuple ):
-                dv = self.klass( *dv )
-
-            if not isinstance( dv, self.klass ):
-                return super( HasMargin, self ).get_default_value()
-
-            self.default_value_type = dvt = 7
-            dv = ( self.klass, (), dv.get() )
-
-        return ( dvt, dv )
-
-#-------------------------------------------------------------------------------
-#  'HasBorder' trait:
-#-------------------------------------------------------------------------------
-
-class HasBorder ( HasMargin ):
-    """ Defines a trait whose value must be a Border object or an integer
-        or tuple value that can be converted to one.
-    """
-
-    # The desired value class:
-    klass = Border
-
-    # Define the default value for the trait:
-    default_value = Border( 0 )
-
-    # A description of the type of value this trait accepts:
-    info_text = ('a Border instance, or an integer in the range from 0 to 32 '
-                 'or a tuple with 1, 2 or 4 integers in that range that can be '
-                 'used to define one')
-
-#-------------------------------------------------------------------------------
 #  Other trait definitions:
 #-------------------------------------------------------------------------------
-
-# The position of an image relative to its associated text:
-Position = Enum( 'left', 'right', 'above', 'below' )
-
-# The alignment of text within a control:
-Alignment = Enum( 'default', 'left', 'center', 'right' )
 
 # The spacing between two items:
 Spacing = Range( -32, 32, 3 )
@@ -462,4 +223,3 @@ Spacing = Range( -32, 32, 3 )
 
 # Types that represent sequences:
 SequenceTypes = ( tuple, list )
-
