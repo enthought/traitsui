fedaf08ae2c74070b1a9250fb6951b8ac591a02f
Corran Webster <cwebster@enthought.com>
2016-12-16 09:47:53 +0000

    Rename bootstrap script; move framebuffer setup into main travis.yml

--- travis_ci_bootstrap_edm.sh
+++ travis_ci_bootstrap_edm.sh
@@ -4,9 +4,6 @@ then
     wget https://package-data.enthought.com/edm/osx_x86_64/1.3/edm_1.3.0.pkg
     sudo installer -pkg edm_1.3.0.pkg -target /
 else
-    # set up XVFB for GUI tests
-    sh -e /etc/init.d/xvfb start
-
     # download and install EDM
     wget https://package-data.enthought.com/edm/rh5_x86_64/1.3/edm_1.3.0_linux_x86_64.sh
     chmod u+x edm_1.3.0_linux_x86_64.sh
@@ -15,4 +12,4 @@ else
 fi
 
 # install pip and invoke into default EDM environment
-edm install pip invoke -y
+edm install -y pip invoke
