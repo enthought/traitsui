04482e9ffdbf9f6939df0e77976e0eb7bd5b4a09
Corran Webster <cwebster@enthought.com>
2017-01-16 13:43:20 +0000

    Properly handle dialog lifecycle for simple InstanceEditor.

---
+++ traitsui/tests/editors/test_instance_editor.py
@@ -0,0 +1,49 @@
+from traits.api import HasTraits, Instance, Str
+from traitsui.item import Item
+from traitsui.view import View
+from traitsui.tests._tools import press_ok_button, store_exceptions_on_all_threads, skip_if_not_qt4
+
+
+class EditedInstance(HasTraits):
+    value = Str
+    traits_view = View(Item('value'), buttons=['OK'])
+
+
+class NonmodalInstanceEditor(HasTraits):
+    inst = Instance(EditedInstance, ())
+    traits_view = View(Item('inst', style='simple'), buttons=['OK'])
+
+
+@skip_if_not_qt4
+def test_simple_editor():
+    from pyface import qt
+
+    with store_exceptions_on_all_threads():
+        obj = NonmodalInstanceEditor()
+        ui = obj.edit_traits()
+        editor = ui.get_editors('inst')[0]
+
+        # make the dialog appear
+        editor._button.click()
+
+        # close the ui dialog
+        press_ok_button(editor._dialog_ui)
+
+        # close the main ui
+        press_ok_button(ui)
+
+
+@skip_if_not_qt4
+def test_simple_editor_parent_closed():
+    from pyface import qt
+
+    with store_exceptions_on_all_threads():
+        obj = NonmodalInstanceEditor()
+        ui = obj.edit_traits()
+        editor = ui.get_editors('inst')[0]
+
+        # make the dialog appear
+        editor._button.click()
+
+        # close the main ui
+        press_ok_button(ui)
