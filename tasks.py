23d8f9e5b489c341a4e31ad4397fe1055d6cf556
Ioannis Tziakos <mail@itziakos.gr>
2017-06-10 00:20:05 +0100

    run qt tests when using pyqt5

--- tasks.py
+++ tasks.py
@@ -160,7 +160,7 @@ def test(runtime, toolkit, environment):
         # run the main test suite
         "edm run -e {environment} -- coverage run -p -m nose.core -v traitsui.tests --nologcapture",
     ]
-    if toolkit in {'pyqt', 'pyside'}:
+    if toolkit in {'pyqt', 'pyside', 'pyqt5'}:
         commands += [
             # run the qt4 toolkit test suite
             "edm run -e {environment} -- coverage run -p -m nose.core -v traitsui.qt4.tests --nologcapture"
