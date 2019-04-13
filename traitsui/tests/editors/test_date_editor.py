0767847ddbaae02c5697c54351f55feb18f75385
Kit Choi <kit@kychoi.org>
2019-04-13 20:50:23 +0100

    Make sure ui dispose is called

--- traitsui/tests/editors/test_date_editor.py
+++ traitsui/tests/editors/test_date_editor.py
@@ -1,3 +1,4 @@
+import contextlib
 import datetime
 import unittest
 
@@ -40,55 +41,59 @@ def multi_select_custom_view():
 class TestDateEditorCustomQt(unittest.TestCase):
 
     def test_single_select_qt4(self):
-        foo, _, editor = self.get_custom_editor(single_select_custom_view)
-
-        date = datetime.date(2018, 2, 3)
-        self.click_date_on_editor(editor, date)
-        self.assertEqual(foo.single_date, date)
+        with self.launch_editor(single_select_custom_view) as (foo, editor):
+            date = datetime.date(2018, 2, 3)
+            self.click_date_on_editor(editor, date)
+            self.assertEqual(foo.single_date, date)
 
     def test_multi_select_dates_on_editor(self):
-        foo, ui, editor = self.get_custom_editor(multi_select_custom_view)
-        dates = [
-            datetime.date(2018, 2, 3), datetime.date(2018, 2, 1)
-        ]
-        for date in dates:
-            self.click_date_on_editor(editor, date)
+        with self.launch_editor(multi_select_custom_view) as (foo, editor):
+            dates = [
+                datetime.date(2018, 2, 3), datetime.date(2018, 2, 1)
+            ]
+            for date in dates:
+                self.click_date_on_editor(editor, date)
 
-        for date in dates:
-            self.check_select_status(editor=editor, date=date, selected=True)
+            for date in dates:
+                self.check_select_status(
+                    editor=editor, date=date, selected=True)
 
-        self.assertEqual(foo.dates, sorted(dates))
+            self.assertEqual(foo.dates, sorted(dates))
 
     def test_multi_select_qt4_styles_reset(self):
-        foo, ui, editor = self.get_custom_editor(multi_select_custom_view)
+        with self.launch_editor(multi_select_custom_view) as (foo, editor):
+            date = datetime.date(2018, 2, 1)
+            self.click_date_on_editor(editor, date)
+            self.check_select_status(editor=editor, date=date, selected=True)
 
-        date = datetime.date(2018, 2, 1)
-        self.click_date_on_editor(editor, date)
-        self.check_select_status(editor=editor, date=date, selected=True)
-
-        self.click_date_on_editor(editor, date)
-        self.check_select_status(editor=editor, date=date, selected=False)
+            self.click_date_on_editor(editor, date)
+            self.check_select_status(editor=editor, date=date, selected=False)
 
     def test_multi_select_qt4_set_model_dates(self):
         # Test setting the dates from the model object.
-        foo, ui, editor = self.get_custom_editor(multi_select_custom_view)
-        foo.dates = [
-            datetime.date(2010, 1, 2),
-            datetime.date(2010, 2, 1),
-        ]
+        with self.launch_editor(multi_select_custom_view) as (foo, editor):
+            foo.dates = [
+                datetime.date(2010, 1, 2),
+                datetime.date(2010, 2, 1),
+            ]
 
-        for date in foo.dates:
-            self.check_select_status(editor=editor, date=date, selected=True)
+            for date in foo.dates:
+                self.check_select_status(
+                    editor=editor, date=date, selected=True)
 
     # --------------------
     # Helper methods
     # --------------------
 
-    def get_custom_editor(self, view_factory):
+    @contextlib.contextmanager
+    def launch_editor(self, view_factory):
         foo = Foo()
         ui = foo.edit_traits(view=view_factory())
         editor, = ui._editors
-        return foo, ui, editor
+        try:
+            yield foo, editor
+        finally:
+            ui.dispose()
 
     def check_select_status(self, editor, date, selected):
         from pyface.qt import QtCore, QtGui
