1e59a61da1a4d35678a7d192d29f01380307cd47
Corran Webster <cwebster@enthought.com>
2019-05-08 16:09:51 +0100

    Bump version.

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ from setuptools import setup, find_packages
 from io import open
 
 MAJOR = 6
-MINOR = 1
+MINOR = 2
 MICRO = 0
 
-IS_RELEASED = True
+IS_RELEASED = False
 
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
