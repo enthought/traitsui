4d234d025c5d0b525d1d3200ce1150510cfea406
Kit Yan Choi <kchoi@enthought.com>
2020-06-03 11:17:23 +0100

    Revert "Experiment removing skips that were added due to test interactions"
    This reverts commit 7466c03f9cabb377d63ea59845224d0a111312cb.

--- traitsui/tests/editors/test_image_enum_editor.py
+++ traitsui/tests/editors/test_image_enum_editor.py
@@ -282,6 +282,10 @@ class TestSimpleImageEnumEditor(unittest.TestCase):
 
         return gui, control
 
+    @unittest.skipIf(
+        is_linux and is_current_backend_qt4(),
+        "Issue enthought/traitsui#854, possible test interactions on Linux"
+    )
     def test_simple_editor_more_cols(self):
         # Smoke test for setting up an editor with more than one column
         enum_edit = EnumModel()
@@ -335,6 +339,10 @@ class TestSimpleImageEnumEditor(unittest.TestCase):
             self.assertEqual(list(control.GetChildren()), [])
 
     @skip_if_not_qt4
+    @unittest.skipIf(
+        is_linux,
+        "Issue enthought/traitsui#854, possible test interactions on Linux"
+    )
     def test_simple_editor_combobox(self):
         enum_edit = EnumModel()
 
