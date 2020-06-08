da4264a9aeafaeba5b95377b1f583dd0335eeda1
Ieva <ievacerny@users.noreply.github.com>
2020-06-08 15:04:23 +0100

    Add more TabularEditor tests (#874)
    * Add more tests for Tabular Editor
    
    * Minor wx TabularEditor fix
    
    * Add brief explanation about Rows flag

--- traitsui/wx/tabular_editor.py
+++ traitsui/wx/tabular_editor.py
@@ -579,7 +579,7 @@ class TabularEditor(Editor):
     def _multi_selected_items_changed(self, event):
         """ Handles the editor's 'multi_selected' trait being modified.
         """
-        values = self.values
+        values = self.value
         try:
             self._multi_selected_rows_items_changed(
                 TraitListEvent(
