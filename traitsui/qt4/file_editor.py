bcc1f99e8367905bcdc1b96da461dc4c0be559e6
Kit Choi <kitchoi@users.noreply.github.com>
2020-06-16 19:14:11 +0100

    Implement dispose for FileEditor (Qt) (#890)
    * Implement dispose for FileEditor
    
    * Shorten variable name
    
    * add store_exceptions_on_all_threads just in case
    
    * Update comment
    
    * Expand the comment
    
    * Expand comment
    
    * Implement dispose for custom file editor
    
    * Tried running with wx, and reference a newly found bug
    
    * Leave disconnecting IconButton signal for another PR
    
    * Remove the button.clicked from the stack of connections made. Both approaches to disconnecting it lead to less maintainable code.
    
    * Remove a file accidentally committed...

--- traitsui/qt4/file_editor.py
+++ traitsui/qt4/file_editor.py
@@ -14,12 +14,13 @@
 from os.path import abspath, splitext, isfile, exists
 
 from pyface.qt import QtCore, QtGui, is_qt5
-from traits.api import List, Event, File, Str, TraitError
+from traits.api import Any, Callable, List, Event, File, Str, TraitError, Tuple
 
 # FIXME: ToolkitEditorFactory is a proxy class defined here just for backward
 # compatibility. The class has been moved to the
 # traitsui.editors.file_editor file.
 from traitsui.editors.file_editor import ToolkitEditorFactory
+from .editor import Editor
 from .text_editor import SimpleEditor as SimpleTextEditor
 from .helper import IconButton
 
@@ -35,6 +36,11 @@ class SimpleEditor(SimpleTextEditor):
         and drop a file onto this control.
     """
 
+    #: List of tuple(signal, slot) to be removed in dispose.
+    #: First item in the tuple is the Qt signal, the second item is the event
+    #: handler.
+    _connections_to_remove = List(Tuple(Any(), Callable()))
+
     def init(self, parent):
         """ Finishes initializing the editor by creating the underlying toolkit
             widget.
@@ -48,15 +54,34 @@ class SimpleEditor(SimpleTextEditor):
 
         if self.factory.auto_set:
             control.textEdited.connect(self.update_object)
+            self._connections_to_remove.append(
+                (control.textEdited, self.update_object)
+            )
         else:
             # Assume enter_set is set, or else the value will never get
             # updated.
             control.editingFinished.connect(self.update_object)
+            self._connections_to_remove.append(
+                (control.editingFinished, self.update_object)
+            )
 
         button = IconButton(QtGui.QStyle.SP_DirIcon, self.show_file_dialog)
         layout.addWidget(button)
 
         self.set_tooltip(control)
+
+    def dispose(self):
+        """ Disposes of the contents of an editor.
+        """
+        while self._connections_to_remove:
+            signal, handler = self._connections_to_remove.pop()
+            signal.disconnect(handler)
+
+        # IconButton.clicked signal should be disconnected here.
+        # (enthought/traitsui#888)
+
+        # skip the dispose from TextEditor (enthought/traitsui#884)
+        Editor.dispose(self)
 
     def update_object(self):
         """ Handles the user changing the contents of the edit control.
@@ -145,6 +170,8 @@ class CustomEditor(SimpleTextEditor):
             widget.
         """
         control = _TreeView(self)
+        control.doubleClicked.connect(self._on_dclick)
+
         self._model = model = QtGui.QFileSystemModel()
 
         current_path = abspath(self.str_value)
@@ -196,6 +223,15 @@ class CustomEditor(SimpleTextEditor):
         else:
             header.setResizeMode(0, QtGui.QHeaderView.ResizeToContents)
         header.setStretchLastSection(False)
+
+    def dispose(self):
+        """ Disposes of the contents of an editor.
+        """
+        if self.control is not None:
+            self.control.doubleClicked.disconnect(self._on_dclick)
+
+        # Skip dispose from simple text editor (enthought/traitsui#884)
+        Editor.dispose(self)
 
     def update_object(self, idx):
         """ Handles the user changing the contents of the edit control.
@@ -254,7 +290,6 @@ class _TreeView(QtGui.QTreeView):
 
     def __init__(self, editor):
         super(_TreeView, self).__init__()
-        self.doubleClicked.connect(editor._on_dclick)
         self._editor = editor
 
     def event(self, event):
