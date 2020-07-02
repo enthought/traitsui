1ccedccd0b9b97682d5e6acedc271d5bf430cfb9
Kit Yan Choi <kchoi@enthought.com>
2020-07-02 18:12:38 +0100

    Fix supported combinations of runtime and toolkit

--- ets-demo/etstool.py
+++ ets-demo/etstool.py
@@ -91,8 +91,7 @@ from contextlib import contextmanager
 import click
 
 supported_combinations = {
-    '3.5': {'pyside2', 'pyqt', 'pyqt5', 'null'},
-    '3.6': {'pyside2', 'pyqt', 'pyqt5', 'wx', 'null'},
+    '3.6': {'pyside', 'pyside2', 'pyqt', 'pyqt5', 'wx', 'null'},
 }
 
 # Default Python version to use in the comamnds below if none is specified.
