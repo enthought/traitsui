2cc1184db26706ab13a490a076159be48a9ccc3e
Jonathan Rocher <jrocher@kbibiopharma.com>
2020-07-12 12:50:22 -0500

    Avoid line wrap by shortening var name.

--- traitsui/qt4/tabular_editor.py
+++ traitsui/qt4/tabular_editor.py
@@ -923,10 +923,9 @@ class _TableView(QtGui.QTableView):
         This affects the column widths when not using auto-sizing.
         """
         if not self._is_resizing:
-            num_columns = len(self._editor.adapter.columns)
-            if self._user_widths is None or \
-                    len(self._user_widths) != num_columns:
-                self._user_widths = [None] * num_columns
+            num_cols = len(self._editor.adapter.columns)
+            if self._user_widths is None or len(self._user_widths) != num_cols:
+                self._user_widths = [None] * num_cols
             self._user_widths[index] = new
             if (self._editor.factory is not None
                     and not self._editor.factory.auto_resize):
