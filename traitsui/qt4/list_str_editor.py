03501ed8879975866869386f713fb379238f1b1f
Kit Yan Choi <kchoi@enthought.com>
2020-07-21 11:19:11 +0100

    Add the same reset for ListStrEditor; there are no warnings from the tests yet though

--- traitsui/qt4/list_str_editor.py
+++ traitsui/qt4/list_str_editor.py
@@ -196,6 +196,9 @@ class _ListStrEditor(Editor):
     def dispose(self):
         """ Disposes of the contents of an editor.
         """
+        self.model.beginResetModel()
+        self.model.endResetModel()
+
         self.context_object.on_trait_change(
             self.update_editor, self.extended_name + "_items", remove=True
         )
