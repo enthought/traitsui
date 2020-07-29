f2768682c8b4dc9b243cc2e02784b41ec07c7d10
Aaron Ayres <aayres@enthought.com>
2020-07-29 09:40:42 -0500

    replacing html tags with markdown

--- traitsui/examples/demo/Advanced/Property_List_demo.py
+++ traitsui/examples/demo/Advanced/Property_List_demo.py
@@ -2,30 +2,30 @@
 #  License: BSD Style.
 
 """
-This demo shows the proper way to create a <b>Property</b> whose value is a
-list, especially when the value of the <b>Property</b> will be used in a user
-interface, such as with the <b>TableEditor</b>.
+This demo shows the proper way to create a **Property** whose value is a
+list, especially when the value of the **Property** will be used in a user
+interface, such as with the **TableEditor**.
 
 Most of the demo is just the machinery to set up the example. The key thing to
-note is the declaration of the <i>people</i> trait:
+note is the declaration of the *people* trait:
 
     people = Property( List, depends_on = 'ticker' )
 
-In this case, by defining the <b>Property</b> as having a value of type
-<b>List</b>, you are ensuring that the computed value of the property will be
-validated using the <b>List</b> type, which in addition to verifying that the
+In this case, by defining the **Property** as having a value of type
+**List**, you are ensuring that the computed value of the property will be
+validated using the **List** type, which in addition to verifying that the
 value is indeed a list, also guarantees that it will be converted to a
-<b>TraitListObject</b>, which is necessary for correct interaction with various
+**TraitListObject**, which is necessary for correct interaction with various
 Traits UI editors in a user interface.
 
-Note also the use of the <i>depends_on</i> metadata to trigger a trait property
-change whenever the <i>ticker</i> trait changes (in this case, it is changed
+Note also the use of the *depends_on* metadata to trigger a trait property
+change whenever the *ticker* trait changes (in this case, it is changed
 every three seconds by a background thread).
 
-Finally, the use of the <i>@cached_property</i> decorator simplifies the
-implementation of the property by allowing the <b>_get_people</b> <i>getter</i>
+Finally, the use of the *@cached_property* decorator simplifies the
+implementation of the property by allowing the **_get_people** *getter*
 method to perform the expensive generation of a new list of people only when
-the <i>ticker</i> event fires, not every time it is accessed.
+the *ticker* event fires, not every time it is accessed.
 """
 
 from random import randint, choice
