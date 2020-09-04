62579ac87dd34afa190017f4a390a35c6e5c62d2
Kit Yan Choi <kchoi@enthought.com>
2020-09-04 09:38:52 +0100

    Update travis CI + wx setup

--- etstool.py
+++ etstool.py
@@ -215,7 +215,7 @@ def install(runtime, toolkit, environment, editable, source):
         else:
             # XXX this is mainly for TravisCI workers; need a generic solution
             commands.append(
-                "edm run -e {environment} -- pip install -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-14.04 wxPython"
+                "edm run -e {environment} -- pip install -f https://extras.wxpython.org/wxPython4/extras/linux/gtk3/ubuntu-16.04/ wxPython"
             )
 
     click.echo("Creating environment '{environment}'".format(**parameters))
