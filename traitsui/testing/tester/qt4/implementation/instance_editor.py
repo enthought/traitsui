a596d0fdaf4ac96a0411091088e6dec62e027ee8
Aaron Ayres <aayres@enthought.com>
2020-09-09 16:38:34 -0500

    initial instance editor implementations (just find nested uis).  Note that I had to make a change to wx isntance editor itself to access ui - this idea was taken from Kits draft branch

---
+++ traitsui/testing/tester/qt4/implementation/instance_editor.py
@@ -0,0 +1,39 @@
+#  Copyright (c) 2005-2020, Enthought, Inc.
+#  All rights reserved.
+#
+#  This software is provided without warranty under the terms of the BSD
+#  license included in LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+#  Thanks for using Enthought open source!
+#
+from traitsui.testing.tester import command, locator
+from traitsui.testing.tester.registry_helper import register_nested_ui_solvers
+from traitsui.qt4.instance_editor import (
+    CustomEditor,
+    SimpleEditor
+)
+
+def _get_netsed_ui_simple(target):
+    if target._dialog_ui is None:
+        target._button.click()
+    return target._dialog_ui
+
+
+def _get_netsed_ui_custom(target):
+    return target._ui
+
+
+def register(registry):
+    """ Register interactions for the given registry.
+
+    If there are any conflicts, an error will occur.
+
+    Parameters
+    ----------
+    registry : TargetRegistry
+        The registry being registered to.
+    """
+    register_nested_ui_solvers(registry, SimpleEditor, _get_netsed_ui_simple)
+    register_nested_ui_solvers(registry, CustomEditor, _get_netsed_ui_custom)
