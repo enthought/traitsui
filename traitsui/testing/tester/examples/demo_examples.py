b5953fbb377e203c0ad841baa644b343f471d6c5
Aaron Ayres <aayres@enthought.com>
2020-09-16 14:24:13 -0500

    adding docstring

--- traitsui/testing/tester/examples/demo_examples.py
+++ traitsui/testing/tester/examples/demo_examples.py
@@ -13,6 +13,23 @@ DEMO = pkg_resources.resource_filename("traitsui", "examples/demo")
 
 
 def load_demo(file_path, variable_name="demo"):
+    """ Loads a demo example from given file_path. Extracts the relevant
+    object via variable_name.
+
+    Parameters
+    ----------
+    file_path : str
+        The file_path of the file to be loaded
+    variable_name : str
+        The key in the global symbol state corresponding to the object of
+        interest for the demo.
+    
+    Returns
+    -------
+    Instance of HasTraits
+        It is expected that this object will have edit_traits called on it,
+        so that the demo can be tested.
+    """
     with open(file_path, "r", encoding="utf-8") as f:
         content = f.read()
     globals_ = globals().copy()
