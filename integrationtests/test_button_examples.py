759dcbaa8eacf46266b18a7a699bfba3838eba58
Aaron Ayres <aayres@enthought.com>
2020-09-17 16:32:41 -0500

    requiring either wx or qt toolkit for button demo test

--- integrationtests/test_button_examples.py
+++ integrationtests/test_button_examples.py
@@ -10,12 +10,19 @@ import os
 import pkg_resources
 import unittest
 
-from traitsui.testing.tester import command, locator, query
+from traitsui.testing.tester import command, query
 from traitsui.testing.tester.examples.load_demo import load_demo
 from traitsui.testing.tester.ui_tester import UITester
 
+from traitsui.tests._tools import (
+    requires_toolkit,
+    ToolkitName,
+)
+
 DEMO = pkg_resources.resource_filename("traitsui", "examples/demo")
 
+
+@requires_toolkit([ToolkitName.qt, ToolkitName.wx])
 class TestButtonExamples(unittest.TestCase):
 
     def test_button_editor_simple_demo(self):
