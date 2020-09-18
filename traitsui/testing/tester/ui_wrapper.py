e140882f7fc293396008bced5114b5a91f363ae4
Kit Yan Choi <kchoi@enthought.com>
2020-09-18 18:44:17 +0100

    Fix import order

--- traitsui/testing/tester/ui_wrapper.py
+++ traitsui/testing/tester/ui_wrapper.py
@@ -11,16 +11,16 @@
 
 from contextlib import contextmanager
 
-from traitsui.testing.tester.exceptions import (
-    InteractionNotSupported,
-    LocationNotSupported,
+from traitsui.testing.exception_handling import (
+    reraise_exceptions as _reraise_exceptions,
 )
-from traitsui.testing.tester import locator
 from traitsui.testing.gui import (
     process_cascade_events as _process_cascade_events,
 )
-from traitsui.testing.exception_handling import (
-    reraise_exceptions as _reraise_exceptions,
+from traitsui.testing.tester import locator
+from traitsui.testing.tester.exceptions import (
+    InteractionNotSupported,
+    LocationNotSupported,
 )
 
 
