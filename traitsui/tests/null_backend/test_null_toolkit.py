ac05454d6b0a8905c021c82846c35534d6300d0a
Kit Yan Choi <kchoi@enthought.com>
2020-09-23 18:32:55 +0100

    Use BaseTestMixin in test_null_toolkit

--- traitsui/tests/null_backend/test_null_toolkit.py
+++ traitsui/tests/null_backend/test_null_toolkit.py
@@ -1,10 +1,16 @@
 import unittest
 
 from traits.api import HasTraits, Int
-from traitsui.tests._tools import requires_toolkit, ToolkitName
+from traitsui.tests._tools import BaseTestMixin, requires_toolkit, ToolkitName
 
 
-class TestNullToolkit(unittest.TestCase):
+class TestNullToolkit(BaseTestMixin, unittest.TestCase):
+
+    def setUp(self):
+        BaseTestMixin.setUp(self)
+
+    def tearDown(self):
+        BaseTestMixin.tearDown(self)
 
     @requires_toolkit([ToolkitName.null])
     def test_configure_traits_error(self):
