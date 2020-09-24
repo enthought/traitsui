bc2814624f1c12ff1c4af8e9de06fd03b0e5a557
Kit Yan Choi <kchoi@enthought.com>
2020-09-24 15:35:18 +0100

    Flake8 line too long

--- traitsui/testing/tester/_ui_tester_registry/_common_ui_targets.py
+++ traitsui/testing/tester/_ui_tester_registry/_common_ui_targets.py
@@ -36,7 +36,8 @@ class _BaseSourceWithLocation:
     # the handlers we want to register for the given source_class
     # must be given as a list of tuples where the first element is the
     # interaction class (e.g. command.MouseClick) and the second is the
-    # actual handler function.  See registry.TargetRegistry.register_interaction
+    # actual handler function.
+    # See registry.TargetRegistry.register_interaction
     # for the signature of the callable.
     handlers = []
 
