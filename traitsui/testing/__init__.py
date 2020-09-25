f8142404cd0f9ad353a0b4320e713acba34f2e9d
Kit Yan Choi <kchoi@enthought.com>
2020-09-25 17:07:58 +0100

    More language improvements

--- traitsui/testing/__init__.py
+++ traitsui/testing/__init__.py
@@ -10,7 +10,8 @@
 #
 
 """
-Package providing functionality to help GUI testing with TraitsUI.
+This package provide functionality to help testing GUI applications built
+using TraitsUI.
 
 The top-level ``api`` module exposes the public API for external projects.
 
