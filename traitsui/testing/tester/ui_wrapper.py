1ac140f01a50639dcde31bf899e49f52b2b282b4
Kit Yan Choi <kchoi@enthought.com>
2020-09-25 19:50:51 +0100

    Run event process prior to resolving locations so GUI state is consistent

--- traitsui/testing/tester/ui_wrapper.py
+++ traitsui/testing/tester/ui_wrapper.py
@@ -310,6 +310,8 @@ class UIWrapper:
             except LocationNotSupported as e:
                 supported |= set(e.supported)
             else:
+                with _reraise_exceptions():
+                    _process_cascade_events()
                 return handler(self, location)
 
         raise LocationNotSupported(
