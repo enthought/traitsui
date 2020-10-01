5a909dd874376cca42b0533de514830fdbb773c7
Kit Yan Choi <kchoi@enthought.com>
2020-10-01 17:08:45 +0100

    Add a test for InstanceEditor with kind set to modal

--- traitsui/tests/editors/test_instance_editor.py
+++ traitsui/tests/editors/test_instance_editor.py
@@ -1,8 +1,8 @@
 import unittest
 
+from pyface.toolkit import toolkit_object
 from traits.api import HasTraits, Instance, Str
-from traitsui.item import Item
-from traitsui.view import View
+from traitsui.api import InstanceEditor, Item, View
 from traitsui.tests._tools import (
     BaseTestMixin,
     requires_toolkit,
@@ -16,13 +16,18 @@ from traitsui.testing.api import (
     UITester
 )
 
+ModalDialogTester = toolkit_object(
+    "util.modal_dialog_tester:ModalDialogTester"
+)
+no_modal_dialog_tester = ModalDialogTester.__name__ == "Unimplemented"
+
 
 class EditedInstance(HasTraits):
     value = Str()
     traits_view = View(Item("value"), buttons=["OK"])
 
 
-class NonmodalInstanceEditor(HasTraits):
+class ObjectWithInstance(HasTraits):
     inst = Instance(EditedInstance, ())
 
 
@@ -40,7 +45,7 @@ class TestInstanceEditor(BaseTestMixin, unittest.TestCase):
         BaseTestMixin.tearDown(self)
 
     def test_simple_editor(self):
-        obj = NonmodalInstanceEditor()
+        obj = ObjectWithInstance()
         tester = UITester()
         with tester.create_ui(obj, dict(view=get_view("simple"))) as ui:
             instance = tester.find_by_name(ui, "inst")
@@ -50,7 +55,7 @@ class TestInstanceEditor(BaseTestMixin, unittest.TestCase):
             self.assertEqual(obj.inst.value, "abc")
 
     def test_custom_editor(self):
-        obj = NonmodalInstanceEditor()
+        obj = ObjectWithInstance()
         tester = UITester()
         with tester.create_ui(obj, dict(view=get_view("custom"))) as ui:
             value_txt = tester.find_by_name(ui, "inst").find_by_name("value")
@@ -59,7 +64,7 @@ class TestInstanceEditor(BaseTestMixin, unittest.TestCase):
 
     def test_custom_editor_resynch_editor(self):
         edited_inst = EditedInstance(value='hello')
-        obj = NonmodalInstanceEditor(inst=edited_inst)
+        obj = ObjectWithInstance(inst=edited_inst)
         tester = UITester()
         with tester.create_ui(obj, dict(view=get_view("custom"))) as ui:
             value_txt = tester.find_by_name(ui, "inst").find_by_name("value")
@@ -71,7 +76,7 @@ class TestInstanceEditor(BaseTestMixin, unittest.TestCase):
 
     def test_simple_editor_resynch_editor(self):
         edited_inst = EditedInstance(value='hello')
-        obj = NonmodalInstanceEditor(inst=edited_inst)
+        obj = ObjectWithInstance(inst=edited_inst)
         tester = UITester()
         with tester.create_ui(obj, dict(view=get_view("simple"))) as ui:
             instance = tester.find_by_name(ui, "inst")
@@ -85,8 +90,46 @@ class TestInstanceEditor(BaseTestMixin, unittest.TestCase):
             self.assertEqual(displayed, "bye")
 
     def test_simple_editor_parent_closed(self):
-        obj = NonmodalInstanceEditor()
+        obj = ObjectWithInstance()
         tester = UITester()
         with tester.create_ui(obj, dict(view=get_view('simple'))) as ui:
             instance = tester.find_by_name(ui, "inst")
             instance.perform(MouseClick())
+
+    @unittest.skipIf(no_modal_dialog_tester, "ModalDialogTester unavailable")
+    def test_simple_editor_modal(self):
+        # Test launching the instance editor with kind set to 'modal'
+        obj = ObjectWithInstance()
+        ui_tester = UITester()
+        view = View(
+            Item("inst", style="simple", editor=InstanceEditor(kind="modal"))
+        )
+
+        with ui_tester.create_ui(obj, dict(view=view)) as ui:
+
+            def click_button():
+                ui_tester.find_by_name(ui, "inst").perform(MouseClick())
+
+            def when_opened(tester):
+                with tester.capture_error():
+                    try:
+                        dialog_ui = tester.get_dialog_widget()._ui
+                        # If process_events was not set to false, ``perform``
+                        # will block due to race condition with
+                        # ModalDialogTester.
+                        ui_tester = UITester(process_events=False)
+                        value = ui_tester.find_by_name(dialog_ui, "value")
+                        value.perform(KeySequence("Hello"))
+                        self.assertEqual(obj.inst.value, "")
+                        ok_button = ui_tester.find_by_id(dialog_ui, "OK")
+                        ok_button.perform(MouseClick())
+                    finally:
+                        # If the block above fails, the dialog will block
+                        # forever. Close it regardless.
+                        if tester.get_dialog_widget() is not None:
+                            tester.close(accept=True)
+
+            mdtester = ModalDialogTester(click_button)
+            mdtester.open_and_run(when_opened=when_opened)
+            self.assertTrue(mdtester.dialog_was_opened)
+            self.assertEqual(obj.inst.value, "Hello")
