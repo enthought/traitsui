f9f7ff0084c79a557d3f2a90ae9fd579609d771e
Aaron Ayres <aayres@enthought.com>
2020-10-16 11:26:34 -0500

    updating PRERELEASE to rc2 and setting IS_RELEASED to True

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ from io import open
 MAJOR = 7
 MINOR = 1
 MICRO = 0
-PRERELEASE = ""
-IS_RELEASED = False
+PRERELEASE = "rc2"
+IS_RELEASED = True
 
 # If this file is part of a Git export (for example created with "git archive",
 # or downloaded from GitHub), ARCHIVE_COMMIT_HASH gives the full hash of the
