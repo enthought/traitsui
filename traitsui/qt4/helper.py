236287855b7c9d689680dadaee9f8c09d811bc87
Kit Choi <kitchoi@users.noreply.github.com>
2020-10-19 15:17:16 +0100

    Fix Qt IconButton with a clickable area too small (#1383)
    * Fix IconButton with a clickable area too small.
    
    If we set the maximum size using the icon size, the clickable area
    ends up being too small to click (tested on OSX with FileEditor,
    RangeEditor, ListEditor). If we don't set the maximum size, the
    button takes up too much space. This sizeHint logic is similar
    to how QToolButton.sizeHint is implemented.
    
    * Expand docstring for sizeHint
    
    * Call ensurePolished as it was called in the original implementation
    
    (cherry picked from commit 8912de35f3adb32a3819f774fcf69c5a90dd3ea5)

--- traitsui/qt4/helper.py
+++ traitsui/qt4/helper.py
@@ -173,12 +173,29 @@ class IconButton(QtGui.QPushButton):
 
         # Configure the button.
         self.setIcon(ico)
-        self.setMaximumSize(ico_sz, ico_sz)
         self.setFlat(True)
         self.setFocusPolicy(QtCore.Qt.NoFocus)
 
         self.clicked.connect(slot)
 
+    def sizeHint(self):
+        """ Reimplement sizeHint to return a recommended button size based on
+        the size of the icon.
+
+        Returns
+        -------
+        size : QtCore.QSize
+        """
+        self.ensurePolished()
+
+        # We want the button to have a size similar to the icon.
+        # Using the size computed for a tool button gives a desirable size.
+        option = QtGui.QStyleOptionButton()
+        self.initStyleOption(option)
+        size = self.style().sizeFromContents(
+            QtGui.QStyle.CT_ToolButton, option, option.iconSize
+        )
+        return size
 
 # ------------------------------------------------------------------------
 # Text Rendering helpers
