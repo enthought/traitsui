4f4a06209ec561d622634186b68edd4828d42cd6
Kit Choi <kitchoi@users.noreply.github.com>
2020-10-19 14:57:25 +0100

    Update message to mention previous plan

--- traitsui/image/image.py
+++ traitsui/image/image.py
@@ -5,6 +5,6 @@ from warnings import warn
 warn(
     DeprecationWarning(
         "traitsui.image has been moved to pyface.image and will be removed in "
-        "TraitsUI version 7.2"
+        "TraitsUI version 7.2 (originally intended to be removed in 6.0)"
     )
 )
