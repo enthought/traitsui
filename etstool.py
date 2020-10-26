96f65c8a86af11d8889eada5d4151c98e05dd54a
Sai Rahul Poruri <rporuri@enthought.com>
2020-10-26 09:43:34 +0000

    DEV : Install pyface using edm
    instead of installing it from git source
    
    	modified:   ci-src-requirements.txt
    	modified:   etstool.py

--- etstool.py
+++ etstool.py
@@ -111,8 +111,8 @@ TRAITS_VERSION_REQUIRES = os.environ.get("TRAITS_REQUIRES", "")
 
 # Required runtime dependencies. Should match install_requires in setup.py
 dependencies = {
+    "pyface",
     "traits" + TRAITS_VERSION_REQUIRES,
-    # pyface is installed from source via ci-src-requirements.txt
 }
 
 # NOTE : pyface is always installed from source
