2df37a81ace1f2b70a6349859136911e68ec75b5
Aaron Ayres <aayres@enthought.com>
2021-02-18 21:48:21 -0600

    mention cancel_button is provisional and fix a comment that was too long

--- traitsui/editors/text_editor.py
+++ traitsui/editors/text_editor.py
@@ -74,9 +74,9 @@ class ToolkitEditorFactory(EditorFactory):
     #: Is multi-line text allowed?
     multi_line = Bool(True)
 
-    #: Is editor readonly (will use custom / default editor appearance with readonly flag set to true)
-    #: in contrasrt with readonly style for item when completely another edito
-    #: is used
+    #: Is editor readonly (will use custom / default editor appearance with
+    #: readonly flag set to true) in contrasrt with readonly style for item
+    #: when completely another editor is used
     read_only = Bool(False)
 
     #: Is user input unreadable? (e.g., for a password)
@@ -98,7 +98,9 @@ class ToolkitEditorFactory(EditorFactory):
     placeholder = Str()
 
     #: Whether or not to display a clear button for the text.  This only works
-    #: in the qt>=5.2 backend for simple/text styles of the editor.
+    #: in the qt>=5.2 backend for simple/text styles of the editor.  Note this
+    #: trait is currently provisional and may be replaced in the future by a
+    #: more general feature.
     cancel_button = Bool(False)
 
     # -------------------------------------------------------------------------
