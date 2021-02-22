69099a1610796c8a3e1f3c72e4c14ca1467389a4
Aaron Ayres <aayres@enthought.com>
2021-02-22 14:17:50 -0600

    make suggested change from code review

--- traitsui/qt4/tree_editor.py
+++ traitsui/qt4/tree_editor.py
@@ -1052,21 +1052,25 @@ class SimpleEditor(Editor):
         object = self._data[1]
         items = []
         add = node.get_add(object)
-        if len(add) > 0:
-            for klass in add:
-                prompt = False
-                if isinstance(klass, tuple):
-                    klass, prompt = klass
-                add_node = self._node_for_class(klass)
-                if add_node is not None:
-                    class_name = klass.__name__
-                    name = add_node.get_name(object)
-                    if name == "":
-                        name = class_name
-                    factory = klass
-                    def perform_add(object):
-                        self._menu_new_node(factory, prompt)
-                    items.append(Action(name=name, on_perform=perform_add))
+        # return early if there are no items to be added in the tree
+        if len(add) == 0:
+            return items
+
+        for klass in add:
+            prompt = False
+            if isinstance(klass, tuple):
+                klass, prompt = klass
+            add_node = self._node_for_class(klass)
+            if add_node is None:
+                continue
+            class_name = klass.__name__
+            name = add_node.get_name(object)
+            if name == "":
+                name = class_name
+            factory = klass
+            def perform_add(object):
+                self._menu_new_node(factory, prompt)
+            items.append(Action(name=name, on_perform=perform_add))
         return items
 
     # -------------------------------------------------------------------------
