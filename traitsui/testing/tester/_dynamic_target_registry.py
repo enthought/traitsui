d03049c8b40035ca4fb3e347bc6112a7c84823c6
Kit Yan Choi <kchoi@enthought.com>
2021-03-22 15:33:50 +0000

    Update docstring of DynamicTargetRegistry with adaptations from the abstract class

--- traitsui/testing/tester/_dynamic_target_registry.py
+++ traitsui/testing/tester/_dynamic_target_registry.py
@@ -39,7 +39,28 @@ class DynamicTargetRegistry(AbstractTargetRegistry):
         self.interaction_to_handler = interaction_to_handler
 
     def _get_handler(self, target, interaction):
-        """ Reimplemented AbstractTargetRegistry._get_handler """
+        """ Return a callable for handling an interaction for a given target.
+
+        This is an implementation for an abstract method.
+
+        Parameters
+        ----------
+        target : any
+            The UI target being operated on.
+        interaction : any
+            Any interaction object.
+
+        Returns
+        -------
+        handler : callable(UIWrapper, interaction) -> any
+            The function to handle the given interaction on a target.
+
+        Raises
+        ------
+        InteractionNotSupported
+            If the given target and interaction types are not supported
+            by this registry.
+        """
         if interaction.__class__ not in self._get_interactions(target):
             raise InteractionNotSupported(
                 target_class=target.__class__,
@@ -49,13 +70,46 @@ class DynamicTargetRegistry(AbstractTargetRegistry):
         return self.interaction_to_handler[interaction.__class__]
 
     def _get_interactions(self, target):
-        """ Reimplemented AbstractTargetRegistry._get_interactions """
+        """ Returns all the interactions supported for the given target.
+
+        This is an implementation for an abstract method.
+
+        Parameters
+        ----------
+        target : any
+            The UI target for which supported interactions are queried.
+
+        Returns
+        -------
+        interaction_classes : set
+            Supported interaction types for the given target type.
+        """
         if self.can_support(target):
             return set(self.interaction_to_handler)
         return set()
 
     def _get_interaction_doc(self, target, interaction_class):
-        """ Reimplemented AbstractTargetRegistry._get_interaction_doc """
+        """ Return the documentation for the given target and interaction type.
+
+        This is an implementation for an abstract method.
+
+        Parameters
+        ----------
+        target : any
+            The UI target for which the interaction will be applied.
+        interaction_class : subclass of type
+            Any class.
+
+        Returns
+        -------
+        doc : str
+
+        Raises
+        ------
+        InteractionNotSupported
+            If the given target and interaction types are not supported
+            by this registry.
+        """
         if interaction_class not in self._get_interactions(target):
             raise InteractionNotSupported(
                 target_class=target.__class__,
@@ -65,7 +119,23 @@ class DynamicTargetRegistry(AbstractTargetRegistry):
         return inspect.getdoc(interaction_class)
 
     def _get_solver(self, target, location):
-        """ Reimplemented AbstractTargetRegistry._get_solver """
+        """ Return a callable registered for resolving a location for the
+        given target and location.
+
+        This is an implementation for an abstract method.
+
+        Parameters
+        ----------
+        target : any
+            The UI target being operated on.
+        location : subclass of type
+            The location to be resolved on the target.
+
+        Raises
+        ------
+        LocationNotSupported
+            If the given locator and target types are not supported.
+        """
         raise LocationNotSupported(
             target_class=target.__class__,
             locator_class=location.__class__,
@@ -73,11 +143,43 @@ class DynamicTargetRegistry(AbstractTargetRegistry):
         )
 
     def _get_locations(self, target):
-        """ Reimplemented AbstractTargetRegistry._get_locations """
+        """ Returns all the location types supported for the given target.
+
+        This is an implementation for an abstract method.
+
+        Parameters
+        ----------
+        target : any
+            The UI target for which supported location types are queried.
+
+        Returns
+        -------
+        locators_classes : set
+            Supported locator types for the given target type.
+        """
         return set()
 
     def _get_location_doc(self, target, locator_class):
-        """ Reimplemented AbstractTargetRegistry._get_location_doc """
+        """ Return the documentation for the given target and locator type.
+
+        This is an implementation for an abstract method.
+
+        Parameters
+        ----------
+        target : any
+            The UI target being operated on.
+        locator_class : subclass of type
+            Any class.
+
+        Returns
+        -------
+        doc : str
+
+        Raises
+        ------
+        LocationNotSupported
+            If the given locator and target types are not supported.
+        """
         raise LocationNotSupported(
             target_class=target.__class__,
             locator_class=locator_class,
