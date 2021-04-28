74d2508a0a694815c386173e1460c6e37c68aaa3
aaronayres35 <36972686+aaronayres35@users.noreply.github.com>
2021-04-28 07:00:14 -0500

    Dont crash when failing to add item to list (#1559)
    * fail less aggressively
    
    * add a regression test
    
    * flake8
    
    * move TraitError import to top of module
    
    * use raise_to_debug
    
    * remove redundant skip

--- traitsui/qt4/list_editor.py
+++ traitsui/qt4/list_editor.py
@@ -28,7 +28,7 @@ from pyface.qt import QtCore, QtGui
 
 from pyface.api import ImageResource
 
-from traits.api import Str, Any, Bool, Dict, Instance, List
+from traits.api import Str, Any, Bool, Dict, Instance, List, TraitError
 from traits.trait_base import user_name_for, xgetattr
 
 from traitsui.editors.list_editor import ListItemProxy
@@ -308,7 +308,13 @@ class SimpleEditor(Editor):
         index += offset
         item_trait = self._trait_handler.item_trait
         value = item_trait.default_value_for(self.object, self.name)
-        self.value = list[:index] + [value] + list[index:]
+        try:
+            self.value = list[:index] + [value] + list[index:]
+        # if the default new item is invalid, we just don't add it to the list.
+        # traits will still give an error message, but we don't want to crash
+        except TraitError:
+            from traitsui.api import raise_to_debug
+            raise_to_debug()
         self.update_editor()
 
     def add_before(self):
