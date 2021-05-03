37d22915efc81fa4b7a89a56481824d144cf73f5
Poruri Sai Rahul <rporuri@enthought.com>
2021-05-03 17:22:14 +0530

    CLN : literalinclude wizard and configure_traits_view_buttons
    	modified:   docs/source/traitsui_user_manual/custom_view.rst
    	new file:   docs/source/traitsui_user_manual/examples/wizard.py
    	modified:   examples/tutorials/doc_examples/examples/configure_traits_view_buttons.py
    	deleted:    examples/tutorials/doc_examples/examples/wizard.py

---
+++ docs/source/traitsui_user_manual/examples/wizard.py
@@ -0,0 +1,41 @@
+# (C) Copyright 2004-2021 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
+
+# wizard.py -- Example of a traits-based wizard UI
+
+from traits.api import HasTraits, Str
+from traits.etsconfig.api import ETSConfig
+from traitsui.api import Item, View, VGroup
+
+
+class Person(HasTraits):
+    first_name = Str()
+    last_name = Str()
+
+    company = Str()
+    position = Str()
+
+    view = View(
+        VGroup(Item("first_name"), Item("last_name")),
+        VGroup(Item("company"), Item("position")),
+    )
+
+
+person = Person(
+    first_name='Postman',
+    last_name='Pat',
+    company="Enthought",
+    position="Software Developer",
+)
+
+
+if ETSConfig.toolkit == "wx":
+    # Wizard window is currently only available for wx backend.
+    person.configure_traits(kind='wizard')
--- examples/tutorials/doc_examples/examples/configure_traits_view_buttons.py
+++ examples/tutorials/doc_examples/examples/configure_traits_view_buttons.py
@@ -1,16 +1,18 @@
-#  Copyright (c) 2007, Enthought, Inc.
-#  License: BSD Style.
-
+# (C) Copyright 2004-2021 Enthought, Inc., Austin, TX
+# All rights reserved.
+#
+# This software is provided without warranty under the terms of the BSD
+# license included in LICENSE.txt and may be redistributed only under
+# the conditions described in the aforementioned license. The license
+# is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+# Thanks for using Enthought open source!
 
 # configure_traits_view_buttons.py -- Sample code to demonstrate
-#                                     configure_traits()
+# configure_traits()
 
-# --[Imports]--------------------------------------------------------------
 from traits.api import HasTraits, Str, Int
-from traitsui.api import View, Item
-from traitsui.menu import OKButton, CancelButton
-
-# --[Code]-----------------------------------------------------------------
+from traitsui.api import CancelButton, Item, OKButton, View
 
 
 class SimpleEmployee(HasTraits):
@@ -22,10 +24,12 @@ class SimpleEmployee(HasTraits):
     salary = Int()
 
 
-view1 = View(Item(name='first_name'),
-             Item(name='last_name'),
-             Item(name='department'),
-             buttons=[OKButton, CancelButton])
+view1 = View(
+    Item(name='first_name'),
+    Item(name='last_name'),
+    Item(name='department'),
+    buttons=[OKButton, CancelButton],
+)
 
 sam = SimpleEmployee()
 sam.configure_traits(view=view1)
