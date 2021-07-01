52f0508d4f042f007a8a4d5306071cb4078ef17d
Aaron Ayres <aayres@enthought.com>
2021-07-01 13:20:50 -0500

    add start of TreeEditor support for Qt

---
+++ traitsui/testing/tester/_ui_tester_registry/qt4/_traitsui/tree_editor.py
@@ -0,0 +1,114 @@
+#  Copyright (c) 2005-2020, Enthought, Inc.
+#  All rights reserved.
+#
+#  This software is provided without warranty under the terms of the BSD
+#  license included in LICENSE.txt and may be redistributed only
+#  under the conditions described in the aforementioned license.  The license
+#  is also available online at http://www.enthought.com/licenses/BSD.txt
+#
+#  Thanks for using Enthought open source!
+#
+
+from traitsui.qt4.tree_editor import SimpleEditor
+from traitsui.testing.tester.qt4 import helpers
+from traitsui.testing.tester import command, locator, registry_helper, query
+
+from traitsui.testing.tester._ui_tester_registry._common_ui_targets import (
+    BaseSourceWithLocation
+)
+from traitsui.testing.tester._ui_tester_registry._traitsui_ui import (
+    register_traitsui_ui_solvers,
+)
+
+class _SimpleEditorWithTreeNode(BaseSourceWithLocation):
+    source_class = SimpleEditor
+    locator_class = locator.TreeNode
+    handlers = [
+        (command.MouseClick, lambda wrapper, interaction: _mouse_click(delay=wrapper.delay)),
+        (command.KeySequence,
+            lambda wrapper, action: _key_sequence(
+                sequence=action.sequence,
+                delay=wrapper.delay,
+            )),
+        (command.KeyClick,
+            lambda wrapper, action: _key_press(
+                key=action.key,
+                delay=wrapper.delay,
+            )),
+        (query.DisplayedText,
+            lambda wrapper, _:  _get_displayed_text()),
+    ]
+
+    @classmethod
+    def register(cls, registry):
+        """ Class method to register interactions on a
+        _SimpleEditorWithTreeNode for the given registry.
+
+        If there are any conflicts, an error will occur.
+
+        Parameters
+        ----------
+        registry : TargetRegistry
+            The registry being registered to.
+        """
+        super().register(registry)
+        register_traitsui_ui_solvers(
+            registry=registry,
+            target_class=cls,
+            traitsui_ui_getter=lambda target: target._get_nested_ui()
+        )
+
+    def _get_model_view_index(self):
+        tree_view = self.source._tree
+        i_column = self.location.column
+        i_rows = iter(self.location.row)
+        item = tree_view.topLevelItem(next(i_rows))
+        for i_row in i_rows:
+            item = item.child(i_row)
+        q_model_index = tree_view.indexFromItem(item, i_column)
+        return dict(
+            model=tree_view.model(),
+            view=tree_view,
+            index=q_model_index,
+        )
+
+    def _mouse_click(self, delay=0):
+        helpers.mouse_click_item_view(
+            **self._get_model_view_index(),
+            delay=delay,
+        )
+
+    def _mouse_dclick(self, delay=0):
+        helpers.mouse_dclick_item_view(
+            **self._get_model_view_index(),
+            delay=delay,
+        )
+
+    def _key_press(self, key, delay=0):
+        helpers.key_press_item_view(
+            **self._get_model_view_index(),
+            key=key,
+            delay=delay,
+        )
+
+    def _key_sequence(self, sequence, delay=0):
+        helpers.key_sequence_item_view(
+            **self._get_model_view_index(),
+            sequence=sequence,
+            delay=delay,
+        )
+
+    def _get_displayed_text(self):
+        return helpers.get_display_text_item_view(
+            **self._get_model_view_index(),
+        )
+
+    def _get_nested_ui(self):
+        """ Method to get the nested ui corresponding to the List element at
+        the given index.
+        """
+        return self.source._editor._node_ui
+
+
+def register(registry):
+    _SimpleEditorWithTreeNode.register(registry)
