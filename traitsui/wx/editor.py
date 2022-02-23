a7dac4aa5cb15d733117a295bebfeafa10b2c06a
Corran Webster <cwebster@enthought.com>
2022-02-23 09:33:09 +0000

    Use Pyface's message dialogs instead of rolling our own. (#1828)
    This allows us to use the same error-handling code cross-platform, modulo
    a little tweaking for editors which wrap QLayouts instead of QWidgets.
    
    This should be transparent to tests - the underlying widgets are the same, we
    just create them differently.

--- traitsui/wx/editor.py
+++ traitsui/wx/editor.py
@@ -61,17 +61,6 @@ class Editor(UIEditor):
         if self.control.GetValue() != new_value:
             self.control.SetValue(new_value)
 
-    def error(self, excp):
-        """Handles an error that occurs while setting the object's trait value."""
-        dlg = wx.MessageDialog(
-            self.control,
-            str(excp),
-            self.description + " value error",
-            wx.OK | wx.ICON_INFORMATION,
-        )
-        dlg.ShowModal()
-        dlg.Destroy()
-
     def set_tooltip_text(self, control, text):
         """Sets the tooltip for a specified control."""
         control.SetToolTip(text)
