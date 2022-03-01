b7980ea0079bb4904f9598ba9d78cbef01d01418
Corran Webster <cwebster@enthought.com>
2022-03-01 09:56:57 +0000

    Fix `TreeEditor` extended selection bug (#1846)
    * Fix TreeEditor extended selection bug
    
    This should fix #1839.  Can't easily test, and Wx fix is blocked by #1845.
    
    * Add a comment.

--- traitsui/qt4/tree_editor.py
+++ traitsui/qt4/tree_editor.py
@@ -888,6 +888,8 @@ class SimpleEditor(Editor):
                 if id(nid) == id(nids[0]):
                     object = sel_object
                     not_handled = node.select(sel_object)
+                    # need to break here to preserve node value for use later
+                    break
         else:
             nid = None
             object = None
