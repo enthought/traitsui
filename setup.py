0b7351d3020586c11380d5fa9060955ec780a1ec
Corran Webster <cwebster@enthought.com>
2022-11-04 16:08:06 +0000

    Post release version bump (#1958)

--- setup.py
+++ setup.py
@@ -22,9 +22,9 @@ from io import open
 # into the package source.
 MAJOR = 7
 MINOR = 4
-MICRO = 2
+MICRO = 3
 PRERELEASE = ""
-IS_RELEASED = True
+IS_RELEASED = False
 
 # If this file is part of a Git export (for example created with "git archive",
 # or downloaded from GitHub), ARCHIVE_COMMIT_HASH gives the full hash of the
