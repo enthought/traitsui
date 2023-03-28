b6a64978a2d9a204d8329f3ae1defab5991eef4a
Corran Webster <cwebster@enthought.com>
2023-03-28 14:06:29 +0100

    Require up-to-date Pyface, don't duplicate ShadowModuleFinder

--- traitsui/qt4/__init__.py
+++ traitsui/qt4/__init__.py
@@ -20,110 +20,20 @@ import os
 import sys
 import warnings
 
-from packaging.version import Version
-
+try:
+    from pyface.ui import ShadowedModuleFinder
+except ImportError:
+    ShadowedModuleFinder = None
 from traits.etsconfig.api import ETSConfig
 
 
-# Import hooks for loading traitsui.qt.* in place of a traitsui.qt4.*
-# This is just the implementation, it is not connected in this module, but
-# is available for applications which want to install it themselves.
-# It is here rather than in traitsui.qt4 so it can be imported and used
-# without generating the warnings from traitsui.qt4
-#
-# To use manually:
-#
-#     import sys
-#     sys.meta_path.append(ShadowedModuleFinder())
-
-class ShadowedModuleLoader(Loader):
-    """This loads another module into sys.modules with a given name.
-
-    Parameters
-    ----------
-    fullname : str
-        The full name of the module we're trying to import.
-        Eg. "traitsui.qt4.foo"
-    new_name : str
-        The full name of the corresponding "real" module.
-        Eg. "traitsui.qt.foo"
-    new_spec : ModuleSpec instance
-        The spec object for the corresponding "real" module.
-    """
-
-    def __init__(self, fullname, new_name, new_spec):
-        self.fullname = fullname
-        self.new_name = new_name
-        self.new_spec = new_spec
-
-    def create_module(self, spec):
-        """Create the module object.
-
-        This doesn't create the module object directly, rather it gets the
-        underlying "real" module's object, importing it if needed.  This object
-        is then returned as the "new" module.
-        """
-        if self.new_name not in sys.modules:
-            import_module(self.new_name)
-        return sys.modules[self.new_name]
-
-    def exec_module(self, module):
-        """Execute code for the module.
-
-        This is given a module which has already been executed, so we don't
-        need to execute anything.  However we do need to remove the __spec__
-        that the importlibs machinery has injected into the module and
-        replace it with the original spec for the underlying "real" module.
-        """
-        # patch up the __spec__ with the true module's original __spec__
-        if self.new_spec:
-            module.__spec__ = self.new_spec
-            self.new_spec = None
-
-
-class ShadowedModuleFinder(MetaPathFinder):
-    """MetaPathFinder for shadowing modules in a package
-
-    This finds modules with names that match a package but arranges loading
-    from a different package.  By default this is  matches imports from any
-    path starting with traitsui.qt4. and returns a loader which will instead
-    load from traitsui.qt.*
-
-    The end result is that sys.modules has two entries for pointing to the
-    same module object.
-
-    This may be hooked up by code in traitsui.qt4, but it can also be
-    installed manually with::
-
-        import sys
-        sys.meta_path.append(ShadowedModuleFinder())
-
-    Parameters
-    ----------
-    package : str
-        The prefix of the "shadow" package.
-    true_package : str
-        The prefix of the "real" package which contains the actual code.
-    """
-
-    def __init__(self, package="traitsui.qt4.", true_package="traitsui.qt."):
-        self.package = package
-        self.true_package = true_package
-
-    def find_spec(self, fullname, path, target=None):
-        if fullname.startswith(self.package):
-            new_name = fullname.replace(self.package, self.true_package, 1)
-            new_spec = find_spec(new_name)
-            if new_spec is None:
-                return None
-            return ModuleSpec(
-                name=fullname,
-                loader=ShadowedModuleLoader(fullname, new_name, new_spec),
-                is_package=(new_spec.submodule_search_locations is not None),
-            )
-
-
-if any(isinstance(finder, ShadowedModuleFinder) for finder in sys.meta_path):
+if any(
+    (
+        isinstance(finder, ShadowedModuleFinder)
+        and finder.package == "traitsui.qt4."
+    )
+    for finder in sys.meta_path
+):
     # Importing from traitsui.qt4.* is deprecated
     # Already have loader registered.
     warnings.warn(
@@ -136,15 +46,17 @@ future release of Pyface.
         stacklevel=2,
     )
 elif (
-    Version(version('pyface')) < Version('8.0')  # our Pyface is old
-    or os.environ.get('ETS_QT4_IMPORTS', None)  # environment says we want this
+    os.environ.get('ETS_QT4_IMPORTS', None)  # environment says we want this
     or os.environ.get('ETS_TOOLKIT', None) == "qt4"  # environment says old qt4
     or ETSConfig.toolkit == "qt4"  # the ETSConfig toolkit says old qt4
 ):
     # Register our loader.  This is messing with global state that we do not own
     # so we only do it when we have other global state telling us to.
 
-    sys.meta_path.append(ShadowedModuleFinder())
+    sys.meta_path.append(ShadowedModuleFinder(
+        package="traitsui.qt4.",
+        true_package="traitsui.qt.",
+    ))
 
     # Importing from traitsui.qt4.* is deprecated
     warnings.warn(
@@ -167,7 +79,15 @@ Applications which require backwards compatibility can either:
 - set the ETS_QT4_IMPORTS environment variable
 - set the ETS_TOOLKIT environment variable to "qt4",
 - the ETSConfig.toolkit to "qt4"
-- install traitsui.ShadowedModuleFinder() into sys.meta_path
+- install a ShadowedModuleFinder into sys.meta_path::
+
+    import sys
+    from pyface.ui import ShadowedModuleFinder
+
+    sys.meta_path.append(ShadowedModuleFinder(
+        package="traitsui.qt4.",
+        true_package="traitsui.qt.",
+    ))
 """,
         FutureWarning,
         stacklevel=2,
