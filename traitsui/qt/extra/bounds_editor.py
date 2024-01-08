enthought/traitsui

commit 494c4e8a41837a187ef77613c5c36c53ac1b7a4d

Remove deprecated `format` trait from `RangeEditor` (#2002)
Using it will now raise a trait error.

Also removes from the "extra" BoundsEditor.

**Checklist**
- [ ] Add a news fragment if this PR is news-worthy for end users. (see
docs/releases/README.rst)

traitsui/qt/extra/bounds_editor.py
@@ -41,8 +41,6 @@ class _BoundsEditor(Editor):
         self.max = factory.max
         self.min = factory.min
 
-        self.format = factory.format
-
         self.evaluate = factory.evaluate
         self.sync_value(factory.evaluate_name, "evaluate", "from")
 
@@ -53,7 +51,7 @@ class _BoundsEditor(Editor):
         panel = QtGui.QHBoxLayout(self.control)
         panel.setContentsMargins(0, 0, 0, 0)
 
-        self._label_lo = QtGui.QLineEdit(self.format % self.low)
+        self._label_lo = QtGui.QLineEdit(self.format_str % self.low)
         self._label_lo.editingFinished.connect(self.update_low_on_enter)
         panel.addWidget(self._label_lo)
 
@@ -74,7 +72,7 @@ class _BoundsEditor(Editor):
         slider.sliderMoved.connect(self.update_object_on_scroll)
         panel.addWidget(slider)
 
-        self._label_hi = QtGui.QLineEdit(self.format % self.high)
+        self._label_hi = QtGui.QLineEdit(self.format_str % self.high)
         self._label_hi.editingFinished.connect(self.update_high_on_enter)
         panel.addWidget(self._label_hi)
 
@@ -95,14 +93,14 @@ class _BoundsEditor(Editor):
                     low = self.evaluate(low)
             except Exception as ex:
                 low = self.low
-                self._label_lo.setText(self.format % self.low)
+                self._label_lo.setText(self.format_str % self.low)
 
             if not self.factory.is_float:
                 low = int(low)
 
             if low > self.high:
                 low = self.high - self._step_size()
-                self._label_lo.setText(self.format % low)
+                self._label_lo.setText(self.format_str % low)
 
             self.control.slider.setLow(self._convert_to_slider(low))
             self.low = low
@@ -117,14 +115,14 @@ class _BoundsEditor(Editor):
                     high = self.evaluate(high)
             except:
                 high = self.high
-                self._label_hi.setText(self.format % self.high)
+                self._label_hi.setText(self.format_str % self.high)
 
             if not self.factory.is_float:
                 high = int(high)
 
             if high < self.low:
                 high = self.low + self._step_size()
-                self._label_hi.setText(self.format % high)
+                self._label_hi.setText(self.format_str % high)
 
             self.control.slider.setHigh(self._convert_to_slider(high))
             self.high = high
@@ -180,7 +178,7 @@ class _BoundsEditor(Editor):
         if self.control is None:
             return
         if self._label_lo is not None:
-            self._label_lo.setText(self.format % low)
+            self._label_lo.setText(self.format_str % low)
 
         self.control.slider.setLow(self._convert_to_slider(low))
 
@@ -188,7 +186,7 @@ class _BoundsEditor(Editor):
         if self.control is None:
             return
         if self._label_hi is not None:
-            self._label_hi.setText(self.format % high)
+            self._label_hi.setText(self.format_str % high)
 
         self.control.slider.setHigh(self._convert_to_slider(self.high))
 

traitsui/wx/extra/bounds_editor.py
@@ -42,7 +42,7 @@ class _BoundsEditor(Editor):
         self.max = factory.max
         self.min = factory.min
 
-        self.format = factory.format
+        self.format_str = factory.format
 
         self.evaluate = factory.evaluate
         self.sync_value(factory.evaluate_name, "evaluate", "from")
@@ -57,7 +57,7 @@ class _BoundsEditor(Editor):
         self._label_lo = wx.TextCtrl(
             panel,
             -1,
-            self.format % self.low,
+            self.format_str % self.low,
             size=wx.Size(56, 20),
             style=wx.TE_PROCESS_ENTER,
         )
@@ -104,7 +104,7 @@ class _BoundsEditor(Editor):
         self._label_hi = wx.TextCtrl(
             panel,
             -1,
-            self.format % self.high,
+            self.format_str % self.high,
             size=wx.Size(56, 20),
             style=wx.TE_PROCESS_ENTER,
         )
@@ -136,14 +136,14 @@ class _BoundsEditor(Editor):
                     low = self.evaluate(low)
             except Exception as ex:
                 low = self.low
-                self._label_lo.SetValue(self.format % self.low)
+                self._label_lo.SetValue(self.format_str % self.low)
 
             if not self.factory.is_float:
                 low = int(low)
 
             if low > self.high:
                 low = self.high - self._step_size()
-                self._label_lo.SetValue(self.format % low)
+                self._label_lo.SetValue(self.format_str % low)
 
             self.control.lslider.SetValue(self._convert_to_slider(low))
             self.low = low
@@ -160,14 +160,14 @@ class _BoundsEditor(Editor):
                     high = self.evaluate(high)
             except:
                 high = self.high
-                self._label_hi.SetValue(self.format % self.high)
+                self._label_hi.SetValue(self.format_str % self.high)
 
             if not self.factory.is_float:
                 high = int(high)
 
             if high < self.low:
                 high = self.low + self._step_size()
-                self._label_hi.SetValue(self.format % high)
+                self._label_hi.SetValue(self.format_str % high)
 
             self.control.rslider.SetValue(self._convert_to_slider(high))
             self.high = high
@@ -229,7 +229,7 @@ class _BoundsEditor(Editor):
         if self.control is None:
             return
         if self._label_lo is not None:
-            self._label_lo.SetValue(self.format % low)
+            self._label_lo.SetValue(self.format_str % low)
 
         self.control.lslider.SetValue(self._convert_to_slider(low))
 
@@ -237,7 +237,7 @@ class _BoundsEditor(Editor):
         if self.control is None:
             return
         if self._label_hi is not None:
-            self._label_hi.SetValue(self.format % high)
+            self._label_hi.SetValue(self.format_str % high)
 
         self.control.rslider.SetValue(self._convert_to_slider(self.high))
 

traitsui/editors/range_editor.py
@@ -67,11 +67,6 @@ class RangeEditor(EditorFactory):
     #: Formatting string used to format value and labels
     format_str = Str("%s")
 
-    #: Deprecated: Please use ``format_str`` instead.
-    #: See enthought/traitsui#1704
-    #: Formatting string used to format value and labels.
-    format = Property(Str, observe='format_str')
-
     #: Is the range for floating pointer numbers (vs. integers)?
     is_float = Bool(Undefined)
 

traitsui/examples/demo/Dynamic_Forms/dynamic_range_editor.py
@@ -77,7 +77,7 @@ class DynamicRangeEditor(HasPrivateTraits):
                 editor=RangeEditor(
                     low_name='low',
                     high_name='high',
-                    format='%.1f',
+                    format_str='%.1f',
                     label_width=28,
                     mode='auto',
                 ),
@@ -99,7 +99,7 @@ class DynamicRangeEditor(HasPrivateTraits):
                 editor=RangeEditor(
                     low_name='low',
                     high_name='high',
-                    format='%.1f',
+                    format_str='%.1f',
                     label_width=28,
                     mode='xslider',
                 ),
@@ -123,7 +123,7 @@ class DynamicRangeEditor(HasPrivateTraits):
                     high=20,
                     low_name='int_low',
                     high_name='int_high',
-                    format='%d',
+                    format_str='%d',
                     is_float=False,
                     label_width=28,
                     mode='spinner',

traitsui/qt/range_editor.py
@@ -80,9 +80,6 @@ class SimpleSliderEditor(BaseRangeEditor):
     #: High value for the slider range
     high = Any()
 
-    #: Deprecated: This trait is no longer used. See enthought/traitsui#1704
-    format = Str()
-
     def init(self, parent):
         """Finishes initializing the editor by creating the underlying toolkit
         widget.

traitsui/tests/editors/test_range_editor.py
@@ -13,7 +13,7 @@ import unittest
 
 from pyface.constant import OK
 from pyface.toolkit import toolkit_object
-from traits.api import HasTraits, Float, Int, Range
+from traits.api import HasTraits, Float, Int, Range, TraitError
 from traits.testing.api import UnittestTools
 from traitsui.api import Item, RangeEditor, UItem, View
 from traitsui.testing.api import (
@@ -390,14 +390,24 @@ class TestRangeEditor(BaseTestMixin, unittest.TestCase, UnittestTools):
 
     def test_editor_factory_format(self):
         """
+        format trait on RangeEditor editor factory has been removed in
+        favor of format_str.
+        """
+        model = RangeModel()
+        with self.assertRaises(TraitError):
+            view = View(
+                Item("float_value", editor=RangeEditor(format="%s ..."))
+            )
+
+    def test_editor_factory_format_str(self):
+        """
         format trait on RangeEditor editor factory has been deprecated in
         favor of format_str. However, behavior should be unchanged.
         """
         model = RangeModel()
-        with self.assertWarns(DeprecationWarning):
-            view = View(
-                Item("float_value", editor=RangeEditor(format="%s ..."))
-            )
+        view = View(
+            Item("float_value", editor=RangeEditor(format_str="%s ..."))
+        )
         tester = UITester()
         with tester.create_ui(model, dict(view=view)) as ui:
             float_value_field = tester.find_by_name(ui, "float_value")
@@ -406,25 +416,19 @@ class TestRangeEditor(BaseTestMixin, unittest.TestCase, UnittestTools):
                 float_value_text.inspect(DisplayedText()), "0.1 ..."
             )
 
-    def test_editor_format(self):
+    def test_editor_format_str(self):
         """
-        The format trait on an Editor instance previously potentially
-        could override the factory. Now that is not the case.
+        The format trait on an Editor instance has been removed.
         """
         model = RangeModel()
-        with self.assertWarns(DeprecationWarning):
-            view = View(
-                Item("float_value", editor=RangeEditor(format="%s ..."))
-            )
+        view = View(
+            Item("float_value", editor=RangeEditor(format_str="%s ..."))
+        )
         tester = UITester()
         with tester.create_ui(model, dict(view=view)) as ui:
             float_value_field = tester.find_by_name(ui, "float_value")
-            float_value_field._target.format = "%s +++"
-            model.float_value = 0.2
-            float_value_text = float_value_field.locate(Textbox())
-            self.assertEqual(
-                float_value_text.inspect(DisplayedText()), "0.2 ..."
-            )
+            with self.assertRaises(TraitError):
+                float_value_field._target.format = "%s +++"
 
     # regression test for enthought/traitsui#737. Hangs with a popup
     # dialog on wx. (xref: enthought/traitsui#1901)

traitsui/wx/range_editor.py
@@ -74,9 +74,6 @@ class SimpleSliderEditor(BaseRangeEditor):
     #: High value for the slider range
     high = Any()
 
-    #: Deprecated: This trait is no longer used. See enthought/traitsui#1704
-    format = Str()
-
     #: Flag indicating that the UI is in the process of being updated
     ui_changing = Bool(False)
 

commit a00f0c34f51b48745d9fc086b2bc31012f4eb599

Fix regression with `BoundsEditor` introduced when removing deprecated `format` trait (#2025)
Regression introduced in
https://github.com/enthought/traitsui/pull/2002.

traitsui/qt/extra/bounds_editor.py
@@ -25,7 +25,7 @@ class _BoundsEditor(Editor):
     max = Any()
     low = Any()
     high = Any()
-    format = Str()
+    format_str = Str()
 
     def init(self, parent):
         """Finishes initializing the editor by creating the underlying toolkit
@@ -40,6 +40,8 @@ class _BoundsEditor(Editor):
 
         self.max = factory.max
         self.min = factory.min
+
+        self.format_str = factory.format_str
 
         self.evaluate = factory.evaluate
         self.sync_value(factory.evaluate_name, "evaluate", "from")